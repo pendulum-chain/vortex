f797d738dfea2249e7f910412ece0c2c63c11d4d
Marcel Ebert <mail@marcel-ebert.de>
2023-12-08 17:44:26 +0100

    Install mui fonts and icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render } from 'preact';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app';
-import './index.css';
 
 const queryClient = new QueryClient();
 
