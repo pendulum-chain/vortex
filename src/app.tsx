ce715abbb559092c910e0696986c11c57cfe0510
Marcel Ebert <mail@marcel-ebert.de>
2023-12-08 17:35:31 +0100

    Change main page

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,11 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Layout from './components/Layout';
-import { defaultPageLoader } from './components/Loader/Page';
-import { NotFound } from './components/NotFound';
-import { SuspenseLoad } from './components/Suspense';
-import { config } from './config';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import Landing from "./pages/landing";
 
-/**
- * Components need to be default exports inside the file for suspense loading to work properly
- */
-const Dashboard = <SuspenseLoad importFn={() => import('./pages/dashboard/Dashboard')} fallback={defaultPageLoader} />;
 
 export function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Navigate to={config.defaultPage} replace />} />
-        <Route path="/:network/" element={<Layout />}>
-          <Route path="" element={Dashboard} />
-          <Route path="dashboard" element={Dashboard} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <ToastContainer />
-    </>
+    <Routes>
+      <Route path="/" element={<Landing/>}/>
+    </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,15 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render } from 'preact';
-import { Theme } from 'react-daisyui';
-import { BrowserRouter } from 'react-router-dom';
-import { GlobalState, GlobalStateContext, GlobalStateProvider } from './GlobalStateProvider';
-import { NodeInfoProvider } from './NodeInfoProvider';
-import SharedProvider from './SharedProvider';
-import { App } from './app';
-import { emptyFn } from './helpers/general';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {render} from 'preact';
+import {BrowserRouter} from 'react-router-dom';
+import {App} from './app';
 import './index.css';
-import { ThemeName, tenantTheme } from './models/Theme';
 
 const queryClient = new QueryClient();
 
 render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <GlobalStateProvider>
-        <GlobalStateContext.Consumer>
-          {(globalState) => {
-            const { tenantRPC, getThemeName = emptyFn } = globalState as GlobalState;
-            return (
-              <NodeInfoProvider tenantRPC={tenantRPC}>
-                <SharedProvider>
-                  <Theme dataTheme={`${getThemeName()}`} className={tenantTheme[getThemeName() || ThemeName.Pendulum]}>
-                    <App />
-                  </Theme>
-                </SharedProvider>
-              </NodeInfoProvider>
-            );
-          }}
-        </GlobalStateContext.Consumer>
-      </GlobalStateProvider>
+      <App/>
     </BrowserRouter>
   </QueryClientProvider>,
   document.getElementById('app') as HTMLElement,
