bbb4f42abaf9136d7598c762a9006e723a2c6112
Marcel Ebert <mail@marcel-ebert.de>
2023-12-08 18:19:21 +0100

    Add sample component

---
+++ src/components/MyComponent/index.tsx
@@ -0,0 +1,8 @@
+// This is a sample component to keep the folder structure
+// Remove this file when you start working on your own component
+
+function MyComponent() {
+  return <div>My Component</div>;
+}
+
+export default MyComponent;
