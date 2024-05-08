00064994f578b4a5a96833ce96c348d78a847aec
Gianfranco <g.tasteri@gmail.com>
2024-05-08 15:09:04 -0300

    conditionally point to signing backend based on env

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,4 @@
+import { config } from "../config";
 
 export const HORIZON_URL = 'https://horizon.stellar.org';
 //export const HORIZON_URL = "https://horizon-testnet.stellar.org";
@@ -5,4 +6,4 @@ export const BASE_FEE = '1000000';
 export const PENDULUM_WSS = 'wss://rpc-pendulum.prd.pendulumchain.tech';
 //export const PENDULUM_WSS = 'ws://localhost:8000';
 
-export const SIGNING_SERVICE_URL = process.env.PRODUCTION ? ''https://prototype-signer-service.pendulumchain.tech' : 'http://localhost:3000';
+export const SIGNING_SERVICE_URL = config.isProd ? 'https://prototype-signer-service.pendulumchain.tech' : 'http://localhost:3000';
