8fd04ebb55b2d43f23ff35667cbda8e8b3b0b726
b-yap <2826165+b-yap@users.noreply.github.com>
2024-06-17 23:45:59 +0800

    add test case for memos and update memohash

--- signer-service-rust/src/api/sep24Result.rs
+++ signer-service-rust/src/api/sep24Result.rs
@@ -37,10 +37,12 @@ impl Sep24Result {
                     Error::InvalidMemo
                 })?;
 
-                Memo::from_hash_memo::<Hash>(bytes.try_into().unwrap()).map_err(|e| {
+                let bytes: Hash = bytes.try_into().map_err(|e| {
                     error!("‼️{:<6} - Invalid offramp hash memo: {e:?}", "FAILED");
                     Error::InvalidMemo
-                })
+                })?;
+
+                Ok(Memo::MemoHash(bytes))
             }
             _ => {
                 error!("‼️{:<6} - Unsupported offramp memo type: {:?}", "FAILED", self.memo_type);
@@ -56,4 +58,61 @@ impl Sep24Result {
             Error::EncodingFailed("offramping account".to_string())
         })
     }
+}
+
+#[cfg(test)]
+mod test {
+    use base64::Engine;
+    use substrate_stellar_sdk::Memo;
+    use crate::api::Sep24Result;
+    use base64::engine::general_purpose;
+
+    fn test_sep24Result() -> Sep24Result {
+        Sep24Result {
+            amount: "1000000".to_string(),
+            memo: "grJMNS9wmxB3ezp9qATcThV2Ov3LT7IShivbc9ZjLV4=".to_string(),
+            memo_type: "hash".to_string(),
+            offramping_account: "GDNVZLQ4TWVESLVNRWMMH3K6XILRZISHYMOIMGOSYQNRHPXPALX73OM2".to_string(),
+        }
+    }
+
+    #[test]
+    fn test_memo() {
+        let mut sep24 = test_sep24Result();
+
+        // --------------------- test hash memo ---------------------
+        match sep24.memo().expect("should return a hash memo") {
+            Memo::MemoHash(hash) => assert_eq!(
+                general_purpose::STANDARD.encode(&hash),
+                sep24.memo
+            ),
+            _ => assert!(false)
+        }
+
+        sep24.memo = "82b24c352f709b10777b3a7da804dc4e15763afdcb4fb212862bdb73d6632d5e".to_string();
+        assert!(sep24.memo().is_err());
+
+        // --------------------- test text memo ---------------------
+        sep24.memo_type = "text".to_string();
+        // exceeded max characters of 28
+        assert!(sep24.memo().is_err());
+
+        sep24.memo = "testing".to_string();
+        match sep24.memo().expect("should return a text memo") {
+            Memo::MemoText(text) => assert_eq!(
+                std::str::from_utf8(text.get_vec()).expect("should return ok").to_string(),
+                sep24.memo
+            ),
+            _ => assert!(false)
+        }
+
+        // --------------------- test non hash or text memo ---------------------
+        sep24.memo_type = "id".to_string();
+        assert!(sep24.memo().is_err());
+
+        sep24.memo = "".to_string();
+        assert!(sep24.memo().is_err());
+    }
+
+
 }
