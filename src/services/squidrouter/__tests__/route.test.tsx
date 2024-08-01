156144e5975758b0a8090db6c2cd6e0c96e72ddd
Marcel Ebert <mail@marcel-ebert.de>
2024-08-01 16:58:54 +0200

    Fix squidrouter tests

--- src/services/squidrouter/__tests__/route.test.tsx
+++ src/services/squidrouter/__tests__/route.test.tsx
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { getRouteTransactionRequest } from '../route';
-import { TOKEN_CONFIG } from '../../../constants/tokenConfig';
+import { INPUT_TOKEN_CONFIG, InputTokenDetails } from '../../../constants/tokenConfig';
 
 // We need to add a delay to the beforeEach hook to ensure that the test does not run before the SquidRouter API is ready
 beforeEach(() => {
@@ -13,16 +13,17 @@ beforeEach(() => {
 
 describe('Squidrouter', () => {
   describe('should be able to get route for relevant USDC tokens', () => {
-    function getRouteForToken(baseToken: string) {
+    function getRouteForToken(inputToken: InputTokenDetails) {
       const userAddress = '0x7Ba99e99Bc669B3508AFf9CC0A898E869459F877';
+      const ephemeralAddress = '0x7Ba99e99Bc669B3508AFf9CC0A898E869459F877';
       const amount = '1000000000';
 
-      return getRouteTransactionRequest(userAddress, baseToken, amount);
+      return getRouteTransactionRequest(userAddress, amount, ephemeralAddress, inputToken);
     }
 
     it('should successfully query a route for USDC', async () => {
-      const tokenAddress = TOKEN_CONFIG.usdc.erc20AddressNativeChain as string;
-      const route = await getRouteForToken(tokenAddress);
+      const inputToken = INPUT_TOKEN_CONFIG.usdc;
+      const route = await getRouteForToken(inputToken);
 
       expect(route).toBeDefined();
       expect(route.requestId).toBeDefined();
@@ -30,8 +31,8 @@ describe('Squidrouter', () => {
     });
 
     it('should successfully query a route for USDC.e', async () => {
-      const tokenAddress = TOKEN_CONFIG.usdce.erc20AddressNativeChain as string;
-      const route = await getRouteForToken(tokenAddress);
+      const inputToken = INPUT_TOKEN_CONFIG.usdce;
+      const route = await getRouteForToken(inputToken);
 
       expect(route).toBeDefined();
       expect(route.requestId).toBeDefined();
