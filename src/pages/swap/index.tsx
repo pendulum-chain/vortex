de6e735f7bc9c87600c96f586f90235a1b86e424
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-08-05 17:50:02 +0200

    implement styles for signing dialog

--- src/pages/swap/index.tsx
+++ src/pages/swap/index.tsx
@@ -264,7 +264,6 @@ export const SwapPage = () => {
 
   const main = (
     <main ref={formRef}>
-      <DialogBox />
       <form
         className="w-full max-w-2xl px-4 py-8 mx-4 mt-12 mb-12 rounded-lg shadow-custom md:mx-auto md:w-2/3 lg:w-3/5 xl:w-1/2"
         onSubmit={onSubmit}
