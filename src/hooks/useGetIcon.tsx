b6ae3f55412c9884a97918d9add41bbfeca8aa95
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-08-15 13:32:59 +0200

    simplify useGetIcon hook

--- src/hooks/useGetIcon.tsx
+++ src/hooks/useGetIcon.tsx
@@ -6,47 +6,27 @@ import EUR from '../assets/coins/EUR.svg';
 import USDC from '../assets/coins/USDC.png';
 import USDC_POLYGON from '../assets/coins/USDC_POLYGON.svg';
 import DefaultIcon from '../assets/coins/PEN.png';
-import { InputTokenType, OutputTokenType } from '../constants/tokenConfig';
 
-type IconMap = Partial<Record<InputTokenType | OutputTokenType | 'eur', string>>;
-
-const icons: IconMap = {
-  eurc: EURC,
-  eur: EUR,
-  usdc: USDC,
-  usdce: USDC,
+const ICONS = {
+  default: {
+    eurc: EURC,
+    eur: EUR,
+    usdc: USDC,
+    usdce: USDC,
+  },
+  [polygon.id]: {
+    usdc: USDC_POLYGON,
+    usdce: USDC_POLYGON,
+  },
 };
 
-const polygonIcons: IconMap = {
-  usdc: USDC_POLYGON,
-  usdce: USDC_POLYGON,
-};
+type TokenType = keyof typeof ICONS.default | keyof (typeof ICONS)[typeof polygon.id];
 
-const IconMaps: Record<string, IconMap> = {
-  [polygon.id]: polygonIcons,
-  default: icons,
-};
-
-export function useGetIcon(token?: InputTokenType | OutputTokenType | 'eur', defaultIcon = DefaultIcon) {
-  const currentChainId = useChainId();
+export function useGetIcon(token?: TokenType, defaultIcon = DefaultIcon) {
+  const chainId = useChainId();
   const { isConnected } = useAccount();
 
-  const currentIconMap = isConnected ? IconMaps[currentChainId] || IconMaps.default : IconMaps.default;
+  const iconMap = isConnected ? ICONS[chainId as keyof typeof ICONS] : ICONS.default;
 
-  if (!token) return defaultIcon;
-
-  // map(key => key.toLowerCase()) is used to make the comparison case-insensitive
-  const currentChainIconMapHasTokenIcon = Object.keys(currentIconMap)
-    .map((key) => key.toLowerCase())
-    .includes(token.toLowerCase());
-
-  const defaultChainIconMapHasTokenIcon = Object.keys(IconMaps.default)
-    .map((key) => key.toLowerCase())
-    .includes(token.toLowerCase());
-
-  return currentChainIconMapHasTokenIcon
-    ? currentIconMap[token]
-    : defaultChainIconMapHasTokenIcon
-    ? IconMaps.default[token]
-    : defaultIcon;
+  return token ? iconMap[token as keyof typeof iconMap] || ICONS.default[token] || defaultIcon : defaultIcon;
 }
