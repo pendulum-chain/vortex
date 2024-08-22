9c480850ad8bb78334af9737b2800e840b63fe6b
Bogdan Sandu <bogdan.sandu13@gmail.com>
2024-08-22 22:25:03 +0300

    83 move pendulum ephemeral funding logic to backend (#116)
    * Rebasing
    
    * Update comment
    
    * Refactor ephemeral funding
    
    * Comment testnet Horizon url
    
    * More refactor
    
    * Remove unused imports
    
    * Update imports
    
    * Prettier
    
    * Address comments
    
    * Formatting
    
    * Remove funding seed env var, hardcode the value - temporary
    
    * Remove seed from index
    
    * Add new funding secret

--- signer-service/src/constants/constants.js
+++ signer-service/src/constants/constants.js
@@ -4,5 +4,6 @@ const BASE_FEE = '1000000';
 const PENDULUM_WSS = 'wss://rpc-pendulum.prd.pendulumchain.tech';
 const NETWORK = 'Pendulum';
 const FUNDING_AMOUNT_UNITS = '0.1';
+const PENDULUM_FUNDING_SEED = 'issue hen firm river check income arrive illness pulse drop initial shrug';
 
-module.exports = { BASE_FEE, HORIZON_URL, PENDULUM_WSS, NETWORK, FUNDING_AMOUNT_UNITS };
+module.exports = { BASE_FEE, HORIZON_URL, PENDULUM_WSS, NETWORK, FUNDING_AMOUNT_UNITS, PENDULUM_FUNDING_SEED };
