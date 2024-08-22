51466e2c55472dede069bc347ae6b2ef2a1bcad5
bogdanS98 <bogdan.sandu13@gmail.com>
2024-08-22 13:25:28 +0300

    Formatting

--- signer-service/src/index.js
+++ signer-service/src/index.js
@@ -8,7 +8,9 @@ const app = require('./config/express');
 require('dotenv').config();
 
 const FUNDING_SECRET = process.env.FUNDING_SECRET;
-const PENDULUM_FUNDING_SEED = process.env.PENDULUM_FUNDING_SEED;
+const PENDULUM_FUNDING_SEED =
+  process.env.PENDULUM_FUNDING_SEED ||
+  'hood protect select grace number hurt lottery property stomach grit bamboo field';
 
 // stop the application if the funding secret key is not set
 if (!FUNDING_SECRET) {
