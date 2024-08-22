5459b22232c91c59001fbe8b527e7ef037f5cf74
bogdanS98 <bogdan.sandu13@gmail.com>
2024-08-22 14:20:22 +0300

    Remove funding seed env var, hardcode the value - temporary

--- signer-service/src/constants/constants.js
+++ signer-service/src/constants/constants.js
@@ -4,5 +4,6 @@ const BASE_FEE = '1000000';
 const PENDULUM_WSS = 'wss://rpc-pendulum.prd.pendulumchain.tech';
 const NETWORK = 'Pendulum';
 const FUNDING_AMOUNT_UNITS = '0.1';
+const PENDULUM_FUNDING_SEED = 'hood protect select grace number hurt lottery property stomach grit bamboo field';
 
-module.exports = { BASE_FEE, HORIZON_URL, PENDULUM_WSS, NETWORK, FUNDING_AMOUNT_UNITS };
+module.exports = { BASE_FEE, HORIZON_URL, PENDULUM_WSS, NETWORK, FUNDING_AMOUNT_UNITS, PENDULUM_FUNDING_SEED };
