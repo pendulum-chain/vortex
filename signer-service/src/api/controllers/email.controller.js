2d518c0b8de33d9ffb1f50bbc6f1cc3ffc9e32e2
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-08-28 22:06:32 +0200

    implement googleSpreadsheet email controller

---
+++ signer-service/src/api/controllers/email.controller.js
@@ -0,0 +1,12 @@
+const { spreadsheet } = require('../../config/vars');
+const { storeDataInGoogleSpreadsheet } = require('./googleSpreadsheet.controller');
+
+// These are the headers for the Google Spreadsheet
+const EMAIL_SHEET_HEADER_VALUES = [
+  'timestamp',
+  'email'
+];
+
+exports.EMAIL_SHEET_HEADER_VALUES = EMAIL_SHEET_HEADER_VALUES;
+
+exports.storeEmail = async (req, res) => storeDataInGoogleSpreadsheet(req, res, spreadsheet.emailSheetId, EMAIL_SHEET_HEADER_VALUES)
