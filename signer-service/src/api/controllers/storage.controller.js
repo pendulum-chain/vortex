c653d61a4d6315e91db04b4f4b92230c46a94770
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-08-28 22:07:02 +0200

    update googleSpreadsheet storage controller

--- signer-service/src/api/controllers/storage.controller.js
+++ signer-service/src/api/controllers/storage.controller.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-
 const { spreadsheet } = require('../../config/vars');
-const { initGoogleSpreadsheet, getOrCreateSheet, appendData } = require('../services/spreadsheet.service');
+const { storeDataInGoogleSpreadsheet } = require('./googleSpreadsheet.controller');
 
 // These are the headers for the Google Spreadsheet
-exports.SHEET_HEADER_VALUES = [
+const SHEET_HEADER_VALUES = [
   'timestamp',
   'polygonAddress',
   'stellarEphemeralPublicKey',
@@ -16,25 +14,7 @@ exports.SHEET_HEADER_VALUES = [
   'stellarCleanupTx',
 ];
 
-exports.storeData = async (req, res, next) => {
-  try {
-    // We expect the data to be an object that matches our schema
-    const data = req.body;
+exports.SHEET_HEADER_VALUES = SHEET_HEADER_VALUES;
 
-    // Try dumping transactions to spreadsheet
-    const sheet = await initGoogleSpreadsheet(spreadsheet.sheetId, spreadsheet.googleCredentials).then((doc) => {
-      return getOrCreateSheet(doc, this.SHEET_HEADER_VALUES);
-    });
+exports.storeData = async (req, res) => storeDataInGoogleSpreadsheet(req, res, spreadsheet.storageSheetId, SHEET_HEADER_VALUES)
 
-    if (sheet) {
-      console.log('Appending data to sheet');
-      await appendData(sheet, data);
-      return res.status(200).json({ message: 'Data stored successfully' });
-    }
-
-    return res.status(500).json({ error: 'Failed to store data. Sheet unavailable.', details: error.message });
-  } catch (error) {
-    console.error('Error in storeData:', error);
-    return res.status(500).json({ error: 'Failed to store data', details: error.message });
-  }
-};
