a46d21ddcb97ef9ffdb47a24adbb442abaf791bf
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-08-30 18:01:51 +0200

    unify parameters case googleSpreadsheet.controller

--- signer-service/src/api/controllers/googleSpreadSheet.controller.js
+++ signer-service/src/api/controllers/googleSpreadSheet.controller.js
@@ -4,13 +4,13 @@ const { spreadsheet } = require('../../config/vars');
 const { initGoogleSpreadsheet, getOrCreateSheet, appendData } = require('../services/spreadsheet.service');
 
 
-exports.storeDataInGoogleSpreadsheet = async (req, res, spreadsheetId, SHEET_HEADER_VALUES) => {
+exports.storeDataInGoogleSpreadsheet = async (req, res, spreadsheetId, sheetHeaderValues) => {
   try {
     // We expect the data to be an object that matches our schema
     const data = req.body;
 
     // Try dumping transactions to spreadsheet
-    const sheet = await initGoogleSpreadsheet(spreadsheetId, spreadsheet.googleCredentials).then((doc) => getOrCreateSheet(doc, SHEET_HEADER_VALUES));
+    const sheet = await initGoogleSpreadsheet(spreadsheetId, spreadsheet.googleCredentials).then((doc) => getOrCreateSheet(doc, sheetHeaderValues));
 
     if (sheet) {
       console.log('Appending data to sheet');
