ddef30f05af3a5642234db4b44b65c073b934500
Gianfranco <g.tasteri@gmail.com>
2024-09-03 10:13:30 -0300

    modify nabla and asset constants to offramp EURC circle

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,7 +5,7 @@ export const HORIZON_URL = 'https://horizon.stellar.org';
 export const BASE_FEE = '1000000';
 export const PENDULUM_WSS = 'wss://rpc-pendulum.prd.pendulumchain.tech';
 //export const PENDULUM_WSS = 'ws://localhost:8000';
-export const NABLA_ROUTER = '6buMJsFCbXpHRyacKTjBn3Jss241b2aA7CZf9tKzKHMJWpcJ';
+export const NABLA_ROUTER = '6dQQoUKQ9LNDCrGMjoZjeHBXsuihSgQiQEgD9Z7VtHR82wfG'; // EURC circle instance
 
 export const TRANSFER_WAITING_TIME_SECONDS = 6000;
 export const SIGNING_SERVICE_URL =
