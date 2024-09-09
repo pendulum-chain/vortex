f253afc7dfc784ea93fe051a3bb8915b943711f6
Gianfranco <g.tasteri@gmail.com>
2024-09-09 14:53:05 -0300

    adjusting summary card

--- src/components/Iframe/index.tsx
+++ src/components/Iframe/index.tsx
@@ -19,27 +19,26 @@ const IframeComponent: React.FC<IframeProps> = ({
     }
   }, [src, cachedSrc]);
   let main = (
-    <div className="flex justify-center items-center w-full h-full p-4">
-      <div className="max-w-lg w-full">
-        <div className="text-center">
-          <h1 className="text-2xl font-bold text-blue-700 mb-4 mt-4">{title}</h1>
-          <SummaryCard /> 
-          <h2 className="text-md mb-4">{subtitle}</h2>
-          <div className="relative pb-[150%] md:pb-0 md:w-[535px] md:h-[500px]"> 
-            <iframe
-              src={src}
-              style={{ border: 'none' }}
-              className="absolute top-0 left-0 w-full h-full"
-              allowFullScreen
-            />
-          </div>
-        </div>
+    <div className="flex flex-col justify-center items-center p-4 w-full">
+    <div className="md:w-[535px] w-full">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold text-blue-700 mb-4 mt-4">{title}</h1>
       </div>
+      <div className="flex justify-center items-center w-full">
+          <SummaryCard />
+      </div>
+      <h2 className="text-md mb-4">{subtitle}</h2>
     </div>
+    <div className="flex justify-center items-center relative w-full pb-[150%] md:pb-0 md:w-[535px] md:h-[500px]">
+      <iframe
+        src={src}
+        style={{ border: 'none' }}
+        className="absolute top-0 left-0 w-full h-full"
+        allowFullScreen
+      />
+    </div>
+  </div>
   );
-
-
-
   return <BaseLayout main={main} />;
 };
 
--- src/components/SummaryCard/index.tsx
+++ src/components/SummaryCard/index.tsx
@@ -10,62 +10,65 @@ export const SummaryCard = () => {
   const iconOut = useGetIcon(assetIconOut);
 
   return (
-    <div className="bg-white p-4 rounded-lg shadow-lg">
-      <div className="grid grid-cols-2 gap-4 mb-2">  
-        <div className="border rounded p-2 grid grid-rows-2 gap-px h-full">
-          <div className="grid place-items-start">
-            <span className="text-xs font-thin">You withdraw</span>
+    <div className="bg-white p-4 rounded-lg shadow-lg w-full">
+      <div className="grid grid-cols-2 gap-4 mb-2">
+        <div className="border rounded p-1 h-auto flex flex-col justify-between space-y-0">
+          <div className="place-self-start">
+            <span className="text-sm font-thin">You withdraw</span>
           </div>
           <div className="flex justify-between items-center w-full">
-            <span className="text-xl font-bold">13</span>
-            <div className="flex items-center">
-              <img src={icon} alt={assetIcon} className="w-5 h-5" />
-              <span className="ml-1">{assetTicker}</span>
+            <span className="text-xl font-medium text-blue-800">13</span>
+            <div className="flex items-center mr-2">
+              <img src={icon} alt={assetIcon} className="w-6 h-6" />
+              <span className="ml-1 text-blue-800">{assetTicker}</span>
             </div>
           </div>
         </div>
-        <div className="border rounded p-2 grid grid-rows-2 gap-px h-full">
-          <div className="grid place-items-start">
-            <span className="text-xs font-thin">You receive</span>
+        <div className="border rounded p-1 h-auto flex flex-col justify-between space-y-0">
+          <div className="place-self-start">
+            <span className="text-sm font-thin ">You receive</span> 
           </div>
           <div className="flex justify-between items-center w-full">
-            <span className="text-xl font-bold">11.44</span>
-            <div className="flex items-center">
-              <img src={iconOut} alt={assetIconOut} className="w-5 h-5" />
-              <span className="ml-1">{assetOutTicker}</span>
+            <span className="text-xl font-medium text-blue-800">11.44</span>
+            <div className="flex items-center mr-2">
+              <img src={iconOut} alt={assetIconOut} className="w-6 h-6" />
+              <span className="ml-1 text-blue-800">{assetOutTicker}</span>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-1  p-1 grid grid-rows-2 gap-px h-auto">  
-          <div className="grid place-items-start">
-            <span className="text-xs font-thin">Your quote</span>
+
+      <div className="grid grid-cols-10 gap-4">
+        <div className="col-span-3 p-1 h-auto flex flex-col justify-between space-y-0 rounded bg-blue-100 bg-opacity-50">
+          <div className="place-self-start">
+            <span className="text-sm font-thin">Your quote</span>
           </div>
-          <div className="flex items-left w-full">
-            <span className="text-lg font-bold mr-1">11.58</span>
+          <div className="flex items-center w-full">
+            <span className="text-sm md:text-lg mr-1">11.58</span>
             <div className="flex items-center">
-              <img src={iconOut} alt={assetIconOut} className="w-4 h-4" />
+              <img src={iconOut} alt={assetIconOut} className="w-4 md:w-5 h-4 md:h-5" />
               <span className="ml-1">{assetOutTicker}</span>
             </div>
           </div>
         </div>
-        <div className="col-span-1  p-1 grid grid-rows-2 gap-px h-auto"> 
-          <div className="grid place-items-start">
-            <span className="text-xs font-thin">Exchange rate</span>
+        
+        <div className="col-span-4 p-1 h-auto flex flex-col justify-between space-y-0">
+          <div className="place-self-start">
+            <span className="text-sm font-thin">Exchange rate</span>
           </div>
           <div className="flex justify-between items-center w-full">
-            <span className="text-lg font-bold">1 = 0.89</span>
+            <span className="text-sm md:text-lg">1 {assetTicker} ≈ 0.89 {assetOutTicker}</span>
           </div>
         </div>
-        <div className="col-span-1   p-1 grid grid-rows-2 gap-px h-auto">  
-          <div className="grid place-items-start">
-            <span className="text-xs font-thin">Offramp fees</span>
+
+        <div className="col-span-3 p-1 h-auto flex flex-col justify-between space-y-0">
+          <div className="place-self-start">
+            <span className="text-sm font-thin">Offramp fees</span>
           </div>
-          <div className="flex items-left w-full">
-            <span className="text-lg font-bold mr-1">0.14</span>
-            <img src={iconOut} alt={assetIconOut} className="w-4 h-4" />
+          <div className="flex items-center w-full">
+            <span className="text-sm md:text-lg mr-1">0.14</span>
+            <img src={iconOut} alt={assetIconOut} className="w-4 md:w-5 h-4 md:h-5" />
             <span className="ml-1">{assetOutTicker}</span>
           </div>
         </div>
