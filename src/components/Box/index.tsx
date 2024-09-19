030477032582edbc4b232544b3e1fbdae46987f0
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-09-19 16:40:36 +0100

    fix Box mobile styles

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -3,7 +3,7 @@ import { FC } from 'preact/compat';
 export const Box: FC<{ className: string }> = ({ children, className }) => (
   <section
     className={
-      'w-full max-w-2xl px-4 py-8 sm:mx-4 mb-12 rounded-lg shadow-custom md:mx-8 md:mx-auto md:w-2/3 lg:w-3/5 xl:w-1/2 ' +
+      'w-full max-w-2xl px-4 py-8 md:mx-4 mb-12 rounded-lg shadow-custom md:mx-8 md:mx-auto md:w-2/3 lg:w-3/5 xl:w-1/2 ' +
       className
     }
   >
