e7635cd855a0464ca236b22323ad7dbd9527761e
Gianfranco <g.tasteri@gmail.com>
2024-09-20 12:19:54 -0300

    add remarked section to summary card:

--- src/components/SummaryCard/index.tsx
+++ src/components/SummaryCard/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'preact/compat';
 import { useGetIcon } from '../../hooks/useGetIcon';
-import { InputTokenType, OutputTokenType, INPUT_TOKEN_CONFIG, OUTPUT_TOKEN_CONFIG} from '../../constants/tokenConfig';
+import { InputTokenType, OutputTokenType, INPUT_TOKEN_CONFIG, OUTPUT_TOKEN_CONFIG } from '../../constants/tokenConfig';
 import Big from 'big.js';
 import { calculateTotalReceive } from '../FeeCollapse';
 import { roundDownToSignificantDecimals } from '../../helpers/parseNumbers';
@@ -13,65 +13,54 @@ export interface SummaryCardProps {
   toAmount: Big;
 }
 
-export const SummaryCard : FC<SummaryCardProps> = ({
-    assetIn,
-    assetOut,
-    fromAmount,
-    toAmount,
-  }) => {
+export const SummaryCard: FC<SummaryCardProps> = ({ assetIn, assetOut, fromAmount, toAmount }) => {
+  const assetInSymbol = INPUT_TOKEN_CONFIG[assetIn].assetSymbol;
+  const assetOutSymbol = OUTPUT_TOKEN_CONFIG[assetOut].fiat.symbol;
 
-    const assetInSymbol = INPUT_TOKEN_CONFIG[assetIn].assetSymbol;
-    const assetOutSymbol = OUTPUT_TOKEN_CONFIG[assetOut].fiat.symbol;
+  const assetInIcon = useGetIcon(INPUT_TOKEN_CONFIG[assetIn].polygonAssetIcon);
+  const assetOutIcon = useGetIcon(OUTPUT_TOKEN_CONFIG[assetOut].fiat.assetIcon);
 
-    const assetInIcon = useGetIcon(INPUT_TOKEN_CONFIG[assetIn].polygonAssetIcon);
-    const assetOutIcon = useGetIcon(OUTPUT_TOKEN_CONFIG[assetOut].fiat.assetIcon);
+  const receiveAmount = calculateTotalReceive(toAmount.toString(), OUTPUT_TOKEN_CONFIG[assetOut]);
 
-    const receiveAmount = calculateTotalReceive( toAmount.toString(), OUTPUT_TOKEN_CONFIG[assetOut]);
+  const approximateExchangeRate = roundDownToSignificantDecimals(toAmount.div(fromAmount), 2).toString();
+  const offrampFees = roundDownToSignificantDecimals(toAmount.sub(receiveAmount), 2).toString();
 
-    const approximateExchangeRate = roundDownToSignificantDecimals(toAmount.div(fromAmount),2).toString();
-    const offrampFees = roundDownToSignificantDecimals(toAmount.sub(receiveAmount), 2).toString();
-
-    return (
-
-      <div className="bg-white p-4 rounded-lg shadow-lg w-full">
-        <div className="grid grid-cols-2 gap-4 mb-2">
-          <UpperSummaryCard
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-lg w-full">
+      <div className="grid grid-cols-2 gap-4 mb-2">
+        <UpperSummaryCard
           label="You withdraw"
           amount={roundDownToSignificantDecimals(fromAmount, 2).toString()}
           icon={assetInIcon}
           symbol={assetInSymbol}
-          />
-          <UpperSummaryCard
-            label="You receive"
-            amount={receiveAmount}
-            icon={assetOutIcon}
-            symbol={assetOutSymbol}
-          />
-        </div>
-
-        <div className="grid grid-cols-10 gap-4">
-            <LowerSummaryCard
-            label="Your quote"
-            amount={roundDownToSignificantDecimals(toAmount, 2).toString()}
-            icon={assetOutIcon}
-            symbol={assetOutSymbol}
-            colSpan={3}
-          />
-          <LowerSummaryCard
-            label="Exchange rate"
-            amount={`1 ${assetInSymbol} ≈ ${approximateExchangeRate} ${assetOutSymbol}`}
-            icon={null} 
-            symbol={null}
-            colSpan={4}
-          />
-          <LowerSummaryCard
-            label="Offramp fees"
-            amount={offrampFees}
-            icon={assetOutIcon}
-            symbol={assetOutSymbol}
-            colSpan={3}
-          />
-        </div>
+        />
+        <UpperSummaryCard label="You receive" amount={receiveAmount} icon={assetOutIcon} symbol={assetOutSymbol} />
       </div>
-    );
+
+      <div className="grid grid-cols-10 gap-4">
+        <LowerSummaryCard
+          label="Your quote"
+          amount={roundDownToSignificantDecimals(toAmount, 2).toString()}
+          icon={assetOutIcon}
+          symbol={assetOutSymbol}
+          colSpan={3}
+          remarked={true}
+        />
+        <LowerSummaryCard
+          label="Exchange rate"
+          amount={`1 ${assetInSymbol} ≈ ${approximateExchangeRate} ${assetOutSymbol}`}
+          icon={null}
+          symbol={null}
+          colSpan={4}
+        />
+        <LowerSummaryCard
+          label="Offramp fees"
+          amount={offrampFees}
+          icon={assetOutIcon}
+          symbol={assetOutSymbol}
+          colSpan={3}
+        />
+      </div>
+    </div>
+  );
 };
