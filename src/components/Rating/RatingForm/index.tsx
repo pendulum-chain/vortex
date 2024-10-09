eeaf8e1be0d31ca9bed6a7b34743fd659fd07860
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-10-09 10:19:47 +0200

    implement RatingForm component

---
+++ src/components/Rating/RatingForm/index.tsx
@@ -0,0 +1,48 @@
+import { motion, AnimatePresence } from 'framer-motion';
+
+// From the highest to the lowest rating - because of ".rating input:hover ~ input" in ./index.css
+const ratings = [5, 4, 3, 2, 1];
+
+interface RatingFormProps {
+  onSubmit: (ratingValue: number) => void;
+  isPending: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  rating: number;
+}
+
+const RatingForm: React.FC<RatingFormProps> = ({ onSubmit, isPending, isSuccess, isError, rating }) => {
+  const isFormSubmitted = isPending || isSuccess || isError;
+
+  const filterRatingsBasedOnUserInput = (r: number) => !isFormSubmitted || (isFormSubmitted && r <= rating);
+
+  return (
+    <>
+      <h2 className="w-full text-lg text-center">
+        {isFormSubmitted ? 'Thank you!' : 'How would you rate our interface?'}
+      </h2>
+      <motion.form
+        className={`rating rating-lg mt-2.5 pb-5 flex flex-row-reverse ${isFormSubmitted ? 'rating-checked' : ''}`}
+        initial="hidden"
+        animate="visible"
+      >
+        <AnimatePresence>
+          <input type="radio" defaultChecked name="rating" className="hidden" />
+          {ratings.filter(filterRatingsBasedOnUserInput).map((ratingValue, index) => (
+            <motion.input
+              className={`transition bg-orange-400 mask mask-star-2 ${index !== 0 ? 'mr-2.5' : ''}`}
+              value={ratingValue}
+              key={ratingValue}
+              type="radio"
+              name="rating"
+              onClick={() => onSubmit(ratingValue)}
+              exit={{ scale: 0, y: 300, rotate: 180 }}
+            />
+          ))}
+        </AnimatePresence>
+      </motion.form>
+    </>
+  );
+};
+
+export default RatingForm;
