d056362946b745fca92eff27c93e874df2e14dd2
Gianfranco <g.tasteri@gmail.com>
2024-10-31 13:17:57 -0300

    attempting fixes for CORS and non reported errors

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,11 @@ Sentry.init({
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  tracePropagationTargets: [
+    'http://localhost:5173/',
+    'http://localhost:4173/',
+    'https://deploy-preview-244--pendulum-pay.netlify.app/',
+  ],
   // Session Replay
   replaysSessionSampleRate: 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
