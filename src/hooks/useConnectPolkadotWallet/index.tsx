1edc8307ca7b80cebd6042561cecb192f86c2241
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-11-21 14:18:07 +0100

    implement useConnectPolkadotWallet hook

---
+++ src/hooks/useConnectPolkadotWallet/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'preact/hooks';
+import { Wallet, getWallets } from '@talismn/connect-wallets';
+import { useMutation } from '@tanstack/react-query';
+
+import { ToastMessage, showToast } from '../../helpers/notifications';
+import { storageService } from '../../services/storage/local';
+import { LocalStorageKeys } from '../useLocalStorage';
+
+const alwaysShowWallets = ['talisman', 'subwallet-js', 'polkadot-js'];
+
+export const useConnectPolkadotWallet = () => {
+  const [selectedWallet, setSelectedWallet] = useState<Wallet | undefined>();
+
+  const wallets = getWallets().filter((wallet) => alwaysShowWallets.includes(wallet.extensionName) || wallet.installed);
+
+  const {
+    mutate: selectWallet,
+    data: accounts,
+    isPending: loading,
+  } = useMutation({
+    mutationFn: async (wallet: Wallet | undefined) => {
+      setSelectedWallet(wallet);
+      if (!wallet) return [];
+      try {
+        await wallet.enable('Vortex');
+
+        if (wallet.installed) {
+          storageService.set(LocalStorageKeys.SELECTED_POLKADOT_WALLET, wallet.extensionName);
+        }
+
+        return wallet.getAccounts();
+      } catch {
+        showToast(ToastMessage.POLKADOT_WALLET_ALREADY_OPEN_PENDING_CONNECTION);
+        return [];
+      }
+    },
+  });
+
+  return { accounts, wallets, selectWallet, loading, selectedWallet };
+};
