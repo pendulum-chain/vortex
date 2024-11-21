4392f0468784cfa92ec63618ddd1c16fc5aa5ba3
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-11-21 14:14:01 +0100

    add SearchInput component

---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,23 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
+import { Dispatch } from 'react';
+
+interface SearchInputProps {
+  set: Dispatch<string>;
+}
+
+export const SearchInput = ({ set, ...p }: SearchInputProps) => (
+  <label className="input input-bordered flex items-center" htmlFor="search">
+    <MagnifyingGlassIcon className="mr-1 size-5 text-neutral-400" />
+    <input
+      className="w-full"
+      type="text"
+      placeholder="Search"
+      name="search"
+      id="search"
+      onChange={(e) => set((e.target as HTMLInputElement).value)}
+      role="presentation"
+      autoComplete="off"
+      {...p}
+    />
+  </label>
+);
