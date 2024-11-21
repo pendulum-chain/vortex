a0213133198bdde19ccb6c2a3af9f85e8e7b01e2
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-11-21 14:20:52 +0100

    change useGetNetworkIcon enum name from NetworkIcons to Networks

--- src/hooks/useGetNetworkIcon.tsx
+++ src/hooks/useGetNetworkIcon.tsx
@@ -1,14 +1,14 @@
 import ASSET_HUB from '../assets/chains/assetHub.svg';
 import POLYGON from '../assets/chains/polygon.svg';
 
-export enum NetworkIcons {
+export enum Networks {
   assetHub = 'assetHub',
   polygon = 'polygon',
 }
 
 export const NETWORK_ICONS = {
-  [NetworkIcons.assetHub]: ASSET_HUB,
-  [NetworkIcons.polygon]: POLYGON,
+  [Networks.assetHub]: ASSET_HUB,
+  [Networks.polygon]: POLYGON,
 };
 
 export type NetworkIconType = keyof typeof NETWORK_ICONS;
