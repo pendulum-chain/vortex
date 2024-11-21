4a4ca3b8ab7d71e314ed991cdd0cabed00b41fda
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-11-21 14:21:19 +0100

    remove unused config related with pendulum-portal

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,16 +1,3 @@
-import { TenantName } from '../models/Tenant';
-import { ThemeName } from '../models/Theme';
-
-type TenantConfig = Record<
-  TenantName,
-  {
-    name: string;
-    rpc: string;
-    theme: ThemeName;
-    explorer: string;
-  }
->;
-
 type Environment = 'development' | 'staging' | 'production';
 const nodeEnv = process.env.NODE_ENV as Environment;
 const maybeSignerServiceUrl = import.meta.env.VITE_SIGNING_SERVICE_URL;
@@ -24,33 +11,6 @@ export const config = {
   isDev: env === 'development',
   maybeSignerServiceUrl,
   alchemyApiKey,
-  defaultPage: '/pendulum/dashboard',
-  tenants: {
-    [TenantName.Amplitude]: {
-      name: 'Amplitude',
-      rpc: 'wss://rpc-amplitude.pendulumchain.tech',
-      theme: ThemeName.Amplitude,
-      explorer: 'https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Frpc-foucoco.pendulumchain.tech#/explorer/query',
-    },
-    [TenantName.Pendulum]: {
-      name: 'Pendulum',
-      rpc: 'wss://rpc-pendulum.prd.pendulumchain.tech',
-      theme: ThemeName.Pendulum,
-      explorer: 'https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Frpc-foucoco.pendulumchain.tech#/explorer/query',
-    },
-    [TenantName.Foucoco]: {
-      name: 'Foucoco',
-      rpc: 'wss://rpc-foucoco.pendulumchain.tech',
-      theme: ThemeName.Amplitude,
-      explorer: 'https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Frpc-foucoco.pendulumchain.tech#/explorer/query',
-    },
-    [TenantName.Local]: {
-      name: 'Local',
-      rpc: 'ws://localhost:9944',
-      theme: ThemeName.Amplitude,
-      explorer: 'https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Frpc-foucoco.pendulumchain.tech#/explorer/query',
-    },
-  } satisfies TenantConfig,
   swap: {
     deadlineMinutes: 60 * 24 * 7, // 1 week
   },
