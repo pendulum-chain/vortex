397bebafd8722c28bda16baeddfc07ff584148c8
Marcel Ebert <mail@marcel-ebert.de>
2024-11-22 16:03:14 +0100

    Deduct fees from outputAmount for `to_amount` in events

--- src/contexts/events.tsx
+++ src/contexts/events.tsx
@@ -1,9 +1,11 @@
 import { createContext } from 'preact';
 import { PropsWithChildren, useCallback, useContext, useEffect, useRef } from 'preact/compat';
+import Big from 'big.js';
 import * as Sentry from '@sentry/react';
 import { useAccount } from 'wagmi';
 import { INPUT_TOKEN_CONFIG, OUTPUT_TOKEN_CONFIG } from '../constants/tokenConfig';
 import { OfframpingState } from '../services/offrampingFlow';
+import { calculateTotalReceive } from '../components/FeeCollapse';
 
 declare global {
   interface Window {
@@ -240,6 +242,6 @@ export function createTransactionEvent(type: TransactionEvent['event'], state: O
     from_asset: INPUT_TOKEN_CONFIG[state.inputTokenType].assetSymbol,
     to_asset: OUTPUT_TOKEN_CONFIG[state.outputTokenType].stellarAsset.code.string,
     from_amount: state.inputAmount.units,
-    to_amount: state.outputAmount.units,
+    to_amount: calculateTotalReceive(Big(state.outputAmount.units), OUTPUT_TOKEN_CONFIG[state.outputTokenType]),
   };
 }
