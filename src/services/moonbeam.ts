4f13b8d436351aa118bc9529ff16abdd65d5db34
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-11-27 09:05:17 +0100

    usePendulumNode instead of polkadotApi in moonbeam.ts

--- src/services/moonbeam.ts
+++ src/services/moonbeam.ts
@@ -6,13 +6,13 @@ import { moonbeam } from '@wagmi/core/chains';
 
 import { OfframpingState } from './offrampingFlow';
 import encodePayload from './squidrouter/payload';
-import { getApiManagerInstance } from './polkadot/polkadotApi';
+import { usePendulumNode } from '../contexts/polkadotNode';
 import { SIGNING_SERVICE_URL } from '../constants/constants';
 import squidReceiverABI from '../../mooncontracts/splitReceiverABI.json';
 import { squidRouterConfig } from './squidrouter/config';
 import { waitUntilTrue } from '../helpers/function';
 import Big from 'big.js';
-import { getRawInputBalance } from './polkadot/ephemeral';
+import { useGetRawInputBalance } from './polkadot/ephemeral';
 
 export const moonbeamConfig = createConfig({
   chains: [moonbeam],
@@ -21,58 +21,66 @@ export const moonbeamConfig = createConfig({
   },
 });
 
-export async function executeXCM(state: OfframpingState): Promise<OfframpingState> {
-  const pendulumApiComponents = await getApiManagerInstance();
-  const apiData = pendulumApiComponents.apiData!;
+export function useExecuteXCM() {
+  const pendulumNode = usePendulumNode();
+  const getRawInputBalanceHook = useGetRawInputBalance();
 
-  const keyring = new Keyring({ type: 'sr25519', ss58Format: apiData.ss58Format });
-  const ephemeralKeypair = keyring.addFromUri(state.pendulumEphemeralSeed);
-
-  const pendulumEphemeralAccountHex = u8aToHex(decodeAddress(ephemeralKeypair.address));
-  const squidRouterPayload = encodePayload(pendulumEphemeralAccountHex);
-
-  const didInputTokenArrivedOnPendulum = async () => {
-    const inputBalanceRaw = await getRawInputBalance(state);
-    return inputBalanceRaw.gt(Big(0));
-  };
-
-  if (!(await didInputTokenArrivedOnPendulum())) {
-    let { moonbeamXcmTransactionHash } = state;
-
-    if (moonbeamXcmTransactionHash === undefined) {
-      const response = await fetch(`${SIGNING_SERVICE_URL}/v1/moonbeam/execute-xcm`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ id: state.squidRouterReceiverId, payload: squidRouterPayload }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error while executing XCM: ${response.statusText}`);
-      }
-
-      try {
-        moonbeamXcmTransactionHash = (await response.json()).hash;
-
-        // We want to store the `moonbeamXcmTransactionHash` immediately in the local storage
-        // and not just after this function call here would usually end (i.e. after the
-        // tokens arrived on Pendulum).
-        // For that reason we return early here and the outer logic of the `useMainProcess` hook
-        // will ensure that this function `executeXCM` will be called again shortly after
-        // where this time `moonbeamXcmTransactionHash` is already defined right at the beginning
-        // of the call
-        return { ...state, moonbeamXcmTransactionHash };
-      } catch (error) {
-        throw new Error(
-          `Error while executing XCM: Could not fetch transaction receipt for hash : ${moonbeamXcmTransactionHash}`,
-        );
-      }
+  return async (state: OfframpingState): Promise<OfframpingState> => {
+    if (!pendulumNode) {
+      throw new Error('Pendulum node not available');
     }
 
-    await waitForTransactionReceipt(moonbeamConfig, { hash: moonbeamXcmTransactionHash, chainId: moonbeam.id });
-    await waitUntilTrue(didInputTokenArrivedOnPendulum, 5000);
-  }
+    const { ss58Format } = pendulumNode;
 
-  return { ...state, phase: 'subsidizePreSwap' };
+    const keyring = new Keyring({ type: 'sr25519', ss58Format });
+    const ephemeralKeypair = keyring.addFromUri(state.pendulumEphemeralSeed);
+
+    const pendulumEphemeralAccountHex = u8aToHex(decodeAddress(ephemeralKeypair.address));
+    const squidRouterPayload = encodePayload(pendulumEphemeralAccountHex);
+
+    const didInputTokenArrivedOnPendulum = async () => {
+      const inputBalanceRaw = await getRawInputBalanceHook(state);
+      return inputBalanceRaw.gt(Big(0));
+    };
+
+    if (!(await didInputTokenArrivedOnPendulum())) {
+      let { moonbeamXcmTransactionHash } = state;
+
+      if (moonbeamXcmTransactionHash === undefined) {
+        const response = await fetch(`${SIGNING_SERVICE_URL}/v1/moonbeam/execute-xcm`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ id: state.squidRouterReceiverId, payload: squidRouterPayload }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error while executing XCM: ${response.statusText}`);
+        }
+
+        try {
+          moonbeamXcmTransactionHash = (await response.json()).hash;
+
+          // We want to store the `moonbeamXcmTransactionHash` immediately in the local storage
+          // and not just after this function call here would usually end (i.e. after the
+          // tokens arrived on Pendulum).
+          // For that reason we return early here and the outer logic of the `useMainProcess` hook
+          // will ensure that this function `executeXCM` will be called again shortly after
+          // where this time `moonbeamXcmTransactionHash` is already defined right at the beginning
+          // of the call
+          return { ...state, moonbeamXcmTransactionHash };
+        } catch (error) {
+          throw new Error(
+            `Error while executing XCM: Could not fetch transaction receipt for hash : ${moonbeamXcmTransactionHash}`,
+          );
+        }
+      }
+
+      await waitForTransactionReceipt(moonbeamConfig, { hash: moonbeamXcmTransactionHash, chainId: moonbeam.id });
+      await waitUntilTrue(didInputTokenArrivedOnPendulum, 5000);
+    }
+
+    return { ...state, phase: 'subsidizePreSwap' };
+  };
 }
 
 export async function isHashRegistered(hash: `0x${string}`): Promise<boolean> {
