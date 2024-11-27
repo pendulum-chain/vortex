400a49a367d4152b0721e584994ce4b1bab6f3d0
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-11-27 09:11:31 +0100

    usePendulumNode instead of polkadotApi in signedTransaction

--- src/services/signedTransactions.ts
+++ src/services/signedTransactions.ts
@@ -1,11 +1,11 @@
 import { Extrinsic } from '@pendulum-chain/api-solang';
 import { ApiPromise, Keyring } from '@polkadot/api';
-import { prepareSpacewalkRedeemTransaction } from './polkadot';
-import { prepareNablaApproveTransaction, prepareNablaSwapTransaction } from './nabla';
+import { usePrepareSpacewalkRedeemTransaction } from './polkadot';
+import { usePrepareNablaApproveTransaction, usePrepareNablaSwapTransaction } from './nabla';
 import { fetchSigningServiceAccountId } from './signingService';
 import { Keypair } from 'stellar-sdk';
 import { setUpAccountAndOperations, stellarCreateEphemeral } from './stellar';
-import { getApiManagerInstance } from './polkadot/polkadotApi';
+import { usePendulumNode } from '../contexts/polkadotNode';
 import { getAccount } from '@wagmi/core';
 import { ExecutionContext, OfframpingState } from './offrampingFlow';
 import { storeDataInBackend } from './storage/remote';
@@ -21,81 +21,86 @@ export function decodeSubmittableExtrinsic(encodedExtrinsic: string, api: ApiPro
 // Creates and signs all required transactions already so they are ready to be submitted.
 // The transactions are stored in the state and the phase is updated to 'squidRouter'.
 // The transactions are also dumped to a Google Spreadsheet.
-export async function prepareTransactions(state: OfframpingState, context: ExecutionContext): Promise<OfframpingState> {
-  if (state.transactions !== undefined) {
-    console.error('Transactions already prepared');
-    return state;
-  }
+export function usePrepareTransactions() {
+  const pendulumNode = usePendulumNode();
+  const prepareSpacewalkRedeemTransaction = usePrepareSpacewalkRedeemTransaction();
+  const prepareNablaApproveTransaction = usePrepareNablaApproveTransaction();
+  const prepareNablaSwapTransaction = usePrepareNablaSwapTransaction();
 
-  const {
-    stellarEphemeralSecret,
-    pendulumEphemeralSeed,
-    outputTokenType,
-    sepResult,
-    inputAmount,
-    outputAmount,
-    inputTokenType,
-    squidRouterReceiverId,
-    squidRouterReceiverHash,
-  } = state;
+  return async (state: OfframpingState, context: ExecutionContext): Promise<OfframpingState> => {
+    if (!pendulumNode) {
+      throw new Error('Pendulum node not available');
+    }
 
-  const spacewalkRedeemTransaction = await prepareSpacewalkRedeemTransaction(state, context);
-  const nablaApproveTransaction = await prepareNablaApproveTransaction(state, context);
-  const nablaSwapTransaction = await prepareNablaSwapTransaction(state, context);
-
-  // Fund Stellar ephemeral only after all other transactions are prepared
-  await stellarCreateEphemeral(stellarEphemeralSecret, outputTokenType);
-  const stellarFundingAccountId = (await fetchSigningServiceAccountId()).stellar.public;
-  const stellarEphemeralKeypair = Keypair.fromSecret(stellarEphemeralSecret);
-  const stellarEphemeralPublicKey = stellarEphemeralKeypair.publicKey();
-  const { offrampingTransaction, mergeAccountTransaction } = await setUpAccountAndOperations(
-    stellarFundingAccountId,
-    stellarEphemeralKeypair,
-    sepResult,
-    outputTokenType,
-  );
-
-  const transactions = {
-    stellarOfframpingTransaction: offrampingTransaction.toEnvelope().toXDR().toString('base64'),
-    stellarCleanupTransaction: mergeAccountTransaction.toEnvelope().toXDR().toString('base64'),
-    spacewalkRedeemTransaction: encodeSubmittableExtrinsic(spacewalkRedeemTransaction),
-    nablaSwapTransaction: encodeSubmittableExtrinsic(nablaSwapTransaction),
-    nablaApproveTransaction: encodeSubmittableExtrinsic(nablaApproveTransaction),
-  };
-
-  const apiManager = await getApiManagerInstance();
-  const { ss58Format } = apiManager.apiData!;
-  const keyring = new Keyring({ type: 'sr25519', ss58Format });
-  const pendulumEphemeralKeypair = keyring.addFromUri(pendulumEphemeralSeed);
-  const pendulumEphemeralPublicKey = pendulumEphemeralKeypair.address;
-
-  // Get the Polygon account connected by the user
-  const polygonAccount = getAccount(context.wagmiConfig);
-  const polygonAddress = polygonAccount.address;
-
-  // Try to store the data in the backend
-  try {
-    const data = {
-      timestamp: new Date().toISOString(),
-      polygonAddress: polygonAddress || '',
-      stellarEphemeralPublicKey,
-      pendulumEphemeralPublicKey,
-      nablaApprovalTx: transactions.nablaApproveTransaction,
-      nablaSwapTx: transactions.nablaSwapTransaction,
-      spacewalkRedeemTx: transactions.spacewalkRedeemTransaction,
-      stellarOfframpTx: transactions.stellarOfframpingTransaction,
-      stellarCleanupTx: transactions.stellarCleanupTransaction,
-      inputAmount: inputAmount.raw.toString(),
-      inputTokenType,
-      outputAmount: outputAmount.raw.toString(),
+    const {
+      stellarEphemeralSecret,
+      pendulumEphemeralSeed,
       outputTokenType,
+      sepResult,
+      inputAmount,
+      outputAmount,
+      inputTokenType,
       squidRouterReceiverId,
       squidRouterReceiverHash,
-    };
-    await storeDataInBackend(data);
-  } catch (error) {
-    console.error('Error storing data', error);
-  }
+    } = state;
 
-  return { ...state, transactions, phase: 'squidRouter' };
+    const spacewalkRedeemTransaction = await prepareSpacewalkRedeemTransaction(state, context);
+    const nablaApproveTransaction = await prepareNablaApproveTransaction(state, context);
+    const nablaSwapTransaction = await prepareNablaSwapTransaction(state, context);
+
+    // Fund Stellar ephemeral only after all other transactions are prepared
+    await stellarCreateEphemeral(stellarEphemeralSecret, outputTokenType);
+    const stellarFundingAccountId = (await fetchSigningServiceAccountId()).stellar.public;
+    const stellarEphemeralKeypair = Keypair.fromSecret(stellarEphemeralSecret);
+    const stellarEphemeralPublicKey = stellarEphemeralKeypair.publicKey();
+    const { offrampingTransaction, mergeAccountTransaction } = await setUpAccountAndOperations(
+      stellarFundingAccountId,
+      stellarEphemeralKeypair,
+      sepResult,
+      outputTokenType,
+    );
+
+    const transactions = {
+      stellarOfframpingTransaction: offrampingTransaction.toEnvelope().toXDR().toString('base64'),
+      stellarCleanupTransaction: mergeAccountTransaction.toEnvelope().toXDR().toString('base64'),
+      spacewalkRedeemTransaction: encodeSubmittableExtrinsic(spacewalkRedeemTransaction),
+      nablaSwapTransaction: encodeSubmittableExtrinsic(nablaSwapTransaction),
+      nablaApproveTransaction: encodeSubmittableExtrinsic(nablaApproveTransaction),
+    };
+
+    const { ss58Format } = pendulumNode;
+    const keyring = new Keyring({ type: 'sr25519', ss58Format });
+    const pendulumEphemeralKeypair = keyring.addFromUri(pendulumEphemeralSeed);
+    const pendulumEphemeralPublicKey = pendulumEphemeralKeypair.address;
+
+    // Get the Polygon account connected by the user
+    const polygonAccount = getAccount(context.wagmiConfig);
+    const polygonAddress = polygonAccount.address;
+
+    // Try to store the data in the backend
+    try {
+      const data = {
+        timestamp: new Date().toISOString(),
+        polygonAddress: polygonAddress || '',
+        stellarEphemeralPublicKey,
+        pendulumEphemeralPublicKey,
+        nablaApprovalTx: transactions.nablaApproveTransaction,
+        nablaSwapTx: transactions.nablaSwapTransaction,
+        spacewalkRedeemTx: transactions.spacewalkRedeemTransaction,
+        stellarOfframpTx: transactions.stellarOfframpingTransaction,
+        stellarCleanupTx: transactions.stellarCleanupTransaction,
+        inputAmount: inputAmount.raw.toString(),
+        inputTokenType,
+        outputAmount: outputAmount.raw.toString(),
+        outputTokenType,
+        squidRouterReceiverId,
+        squidRouterReceiverHash,
+      };
+      await storeDataInBackend(data);
+    } catch (error) {
+      console.error('Error storing data', error);
+    }
+
+    return { ...state, transactions, phase: 'squidRouter' };
+  };
 }
