bb0d50b11c2ebb678b0957abe0f8ce7d3399aa02
Marcel Ebert <mail@marcel-ebert.de>
2024-11-27 17:02:59 +0100

    Fix wrong router contract

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,7 +5,7 @@ export const BASE_FEE = '1000000';
 export const PENDULUM_WSS = 'wss://rpc-pendulum.prd.pendulumchain.tech';
 export const ASSETHUB_WSS = 'wss://polkadot-asset-hub-rpc.polkadot.io';
 export const WALLETCONNECT_ASSETHUB_ID = 'polkadot:68d56f15f85d3136970ec16946040bc1';
-export const NABLA_ROUTER = '6dQQoUKQ9LNDCrGMjoZjeHBXsuihSgQiQEgD9Z7VtHR82wfG'; // EURC circle instance
+export const NABLA_ROUTER = '6gAVVw13mQgzzKk4yEwScMmWiCNyMAunXFJUZonbgKrym81N'; // AssetHub USDC instance
 
 export const SPACEWALK_REDEEM_SAFETY_MARGIN = 0.05;
 export const AMM_MINIMUM_OUTPUT_SOFT_MARGIN = 0.02;
