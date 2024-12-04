778bb3694042f950b0e1c74f5bbf650574d38da2
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-12-04 16:21:39 +0100

    implement tooltip for FeeComparision

--- src/components/FeeComparison/index.tsx
+++ src/components/FeeComparison/index.tsx
@@ -126,7 +126,15 @@ function FeeComparisonTable({
       <div className="flex items-center justify-center w-full mb-3">
         <div className="flex items-center justify-center w-full gap-4">
           <span className="font-bold text-md">
-            Sending {amount.toFixed(2)} {sourceAssetSymbol} <br /> with
+            Sending {amount.toFixed(2)} {sourceAssetSymbol}
+            <div
+              className="tooltip tooltip-primary before:whitespace-pre-wrap before:content-[attr(data-tip)]"
+              data-tip="Quotes are for Polygon, as the providers don't support Asset hub."
+            >
+              (Polygon)
+            </div>
+            <br />
+            with
           </span>
         </div>
         <div className="flex flex-col items-center justify-center w-full">
