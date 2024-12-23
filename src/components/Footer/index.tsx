d82177fbb00ae57752fc4c0bd9df6be868566737
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-12-23 16:40:11 +0100

    update footer styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -42,7 +42,7 @@ const Copyright = () => (
 
 export function Footer() {
   return (
-    <footer className="footer fixed bottom-0 left-0 w-full px-4 pt-2 pb-4 md:px-12 flex items-end justify-between">
+    <footer className="items-end justify-between px-4 pt-2 pb-4 md:px-12 footer">
       <div className="flex flex-col gap-2">
         <Copyright />
         <div className="flex gap-4">
