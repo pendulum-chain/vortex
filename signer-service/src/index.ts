6d5727449d5c7ead91ca878c3464b2723aea1831
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-01-06 18:29:25 +0100

    migrate index.ts

---
+++ signer-service/src/index.ts
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+
+import { config } from './config/vars';
+import logger from './config/logger';
+import app from './config/express';
+import {
+  FUNDING_SECRET,
+  PENDULUM_FUNDING_SEED,
+  MOONBEAM_EXECUTOR_PRIVATE_KEY,
+  CLIENT_DOMAIN_SECRET,
+} from './constants/constants';
+
+const { port, env } = config;
+
+dotenv.config();
+
+// Consider grouping all environment checks into a single function
+const validateRequiredEnvVars = () => {
+  const requiredVars = {
+    FUNDING_SECRET,
+    PENDULUM_FUNDING_SEED,
+    MOONBEAM_EXECUTOR_PRIVATE_KEY,
+    CLIENT_DOMAIN_SECRET,
+  };
+
+  for (const [key, value] of Object.entries(requiredVars)) {
+    if (!value) {
+      logger.error(`${key} not set in the environment variables`);
+      process.exit(1);
+    }
+  }
+};
+
+// Validate environment variables before starting the server
+validateRequiredEnvVars();
+
+// listen to requests
+app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+
+/**
+ * Exports express
+ * @public
+ */
+export default app;
