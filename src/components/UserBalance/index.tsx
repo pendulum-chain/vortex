449825f95da6746c206f722bbe6ef74922848a4e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-01-10 14:37:43 +0100

    Do not show balance when wallet is not connected

--- src/components/UserBalance/index.tsx
+++ src/components/UserBalance/index.tsx
@@ -9,19 +9,21 @@ interface UserBalanceProps {
 export const UserBalance = ({ token, onClick }: UserBalanceProps) => {
   const inputTokenBalance = useInputTokenBalance({ fromToken: token });
 
+  if (inputTokenBalance === undefined) {
+    return <></>;
+  }
+
   return (
     <p className="flex items-end justify-end mt-1">
-      <p className="mr-0.5">Available:</p>
-      {inputTokenBalance === undefined ? (
-        'N/A'
-      ) : (
+      <>
+        <p className="mr-0.5">Available:</p>
         <div
           className="font-medium transition cursor-pointer hover:underline hover:text-black"
           onClick={() => onClick(inputTokenBalance)}
         >
-          <span className="bold">{inputTokenBalance}</span> {token.assetSymbol}
+          {inputTokenBalance} {token.assetSymbol}
         </div>
-      )}
+      </>
     </p>
   );
 };
