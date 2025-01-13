932d4a7d98d3b4f8e2986a19ab6529cefa06e06a
Gianfranco <g.tasteri@gmail.com>
2025-01-13 10:55:24 -0300

    log signature login raw error

--- src/services/anchor/sep10/index.ts
+++ src/services/anchor/sep10/index.ts
@@ -60,6 +60,7 @@ export async function sep10(
       await checkAndWaitForSignature();
     } catch (error) {
       // We must differentiate between user driven rejection and other errors
+      console.log((error as Error).message);
       if ((error as Error).message.includes('User rejected the request')) {
         throw new Error('User rejected sign request');
       }
