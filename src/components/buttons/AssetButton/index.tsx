ede37a736bed9f7a3ef68a720fa292f060eeb373
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-01-16 20:01:54 +0100

    fix display asset icons on mobile

--- src/components/buttons/AssetButton/index.tsx
+++ src/components/buttons/AssetButton/index.tsx
@@ -12,12 +12,12 @@ export function AssetButton({ assetIcon, tokenSymbol, onClick }: AssetButtonProp
 
   return (
     <button
-      className="hover:bg-blue-200 rounded-full min-h-none h-8 flex items-center mt-0.5 border border-blue-700 px-2 py-1 pr-3"
+      className="hover:bg-blue-200 rounded-full h-8 flex items-center mt-0.5 border border-blue-700 px-2 py-1 pr-3"
       onClick={onClick}
       type="button"
     >
       <span className="h-full p-px mr-1 rounded-full">
-        <img src={icon} alt={assetIcon} className="w-auto h-full" />
+        <img src={icon} alt={assetIcon} className="h-full min-h-5 max-w-min" />
       </span>
       <strong className="font-bold text-black">{tokenSymbol}</strong>
       <ChevronDownIcon className="w-6" />
