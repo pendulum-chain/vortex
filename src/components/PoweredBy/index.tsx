eb5969691d8db9fadbe9c99a735a4a004faeb939
Marcel Ebert <mail@marcel-ebert.de>
2025-01-24 18:28:58 +0100

    Use bank logo

--- src/components/PoweredBy/index.tsx
+++ src/components/PoweredBy/index.tsx
@@ -1,8 +1,8 @@
-import satoshipayLogo from '../../assets/logo/satoshipay.svg';
 import MASTERCARD from '../../assets/payments/mastercard.svg';
-import SEPA from '../../assets/payments/sepa.svg';
+import bank from '../../assets/payments/bank.svg';
 import VISA from '../../assets/payments/visa.svg';
 import vortexLogo from '../../assets/logo/blue.svg';
+
 interface ImageProps {
   src: string;
   alt: string;
@@ -11,20 +11,20 @@ interface ImageProps {
 }
 
 const paymentImages = [
-  { src: SEPA, alt: 'SEPA logo', additionalClass: 'h-6' },
+  { src: bank, alt: 'SEPA logo' },
   { src: MASTERCARD, alt: 'Mastercard logo', comingSoon: true },
   { src: VISA, alt: 'Visa logo', comingSoon: true },
 ];
 
 const Image = ({ src, alt, comingSoon, additionalClass }: ImageProps) => (
-  <div className="flex flex-col items-center text-center">
-    <img src={src} alt={alt} className={`h-[18px] ${additionalClass}`} />
+  <div className="flex flex-col items-normal text-center">
+    <img src={src} alt={alt} className={`${additionalClass} ${comingSoon ? 'h-[18px]' : 'h-6'} `} />
     {comingSoon && <div className="text-[7px] w-12 text-blue-700">Coming soon</div>}
   </div>
 );
 
 const ImageList = ({ images }: { images: ImageProps[] }) => (
-  <div className="flex flex-wrap justify-center items-center gap-x-6">
+  <div className="flex flex-wrap justify-center items-normal gap-x-6">
     {images.map((img) => (
       <Image key={img.alt} {...img} />
     ))}
@@ -33,22 +33,12 @@ const ImageList = ({ images }: { images: ImageProps[] }) => (
 
 export function PoweredBy() {
   return (
-    <section className="flex flex-col justify-between gap-y-2.5">
+    <section className="flex flex-col justify-between gap-y-2.5 my-2">
       <ImageList images={paymentImages} />
       <div className="flex items-center justify-center">
         <p className="mr-1 text-sm text-gray-500">Powered by</p>
         <Image src={vortexLogo} alt="Satoshipay" />
       </div>
-      <p className="flex items-center justify-center mr-1 text-sm text-gray-500">
-        <a
-          href="https://satoshipay.io"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex gap-1 text-xs transition hover:opacity-80"
-        >
-          A <img src={satoshipayLogo} alt="Satoshipay" /> Company
-        </a>
-      </p>
     </section>
   );
 }
