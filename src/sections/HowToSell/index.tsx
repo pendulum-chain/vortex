85db997f7b31b7d183733bdb82cf63995aabab1e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-02-06 14:59:24 +0100

    add lines between HowToSell points, improve paddings

--- src/sections/HowToSell/index.tsx
+++ src/sections/HowToSell/index.tsx
@@ -19,21 +19,25 @@ const steps = [
 
 export const HowToSell = () => (
   <section className="relative py-32 overflow-hidden bg-[radial-gradient(at_50%_50%,theme(colors.blue.900),theme(colors.blue.950),theme(colors.blue.950))]">
-    <canvas className="absolute inset-0 w-full h-full" />
     <div className="container relative z-10 px-4 mx-auto mb-12">
       <div className="mb-12 text-center">
-        <p className="text-4xl text-white md:text-5xl">How to sell cryptocurrency online with Vortex Finance</p>
+        <p className="text-4xl text-white md:text-4xl leading-[3rem]">
+          How to sell cryptocurrency online <br className="hidden md:block" /> with Vortex Finance
+        </p>
       </div>
       <div className="relative flex justify-center">
-        <div className="relative grid max-w-4xl grid-cols-1 gap-8 xs:grid-cols-2 lg:grid-cols-4">
-          {steps.map((step) => (
-            <div key={step.id} className="relative z-10 group">
-              <div className="flex flex-col items-center">
-                <div className="flex items-center justify-center w-20 h-20 mb-4 transition-all duration-300 border-2 border-blue-700 rounded-full shadow-lg bg-[radial-gradient(at_50%_50%,theme(colors.black),theme(colors.blue.950),theme(colors.black)_134%)] group-hover:scale-105">
-                  <span className="text-3xl font-bold text-white">{step.id}</span>
+        <div className="relative grid max-w-5xl grid-cols-1 gap-8 xs:grid-cols-2 lg:grid-cols-4">
+          {steps.map((step, index) => (
+            <div key={step.id} className="relative group">
+              {index !== 0 && (
+                <div className="hidden md:block absolute bg-primary w-full h-[1px] top-1/4 -translate-x-1/2 z-[0]"></div>
+              )}
+              <div className="flex flex-col items-center relative z-[1]">
+                <div className="flex items-center justify-center w-18 h-18 mb-4 transition-all duration-300 border-2 border-blue-700 rounded-full shadow-lg bg-[radial-gradient(at_50%_50%,theme(colors.black),theme(colors.blue.950),theme(colors.black)_134%)] group-hover:scale-105">
+                  <span className="text-xl font-bold text-white">0{step.id}</span>
                 </div>
-                <div className="w-48 text-center transition-all duration-300 group-hover:transform group-hover:translate-y-1">
-                  <p className="text-xs text-white/90">{step.text}</p>
+                <div className="text-center transition-all duration-300 group-hover:transform group-hover:translate-y-1">
+                  <p className="text-white/90">{step.text}</p>
                 </div>
               </div>
             </div>
