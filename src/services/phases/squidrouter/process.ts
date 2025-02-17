29d08a476c1474e7e18be9955a7b7a9d78ca95d0
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-02-17 17:36:55 +0100

    refactor squidrouter process, use SAFE wallet logic

--- src/services/phases/squidrouter/process.ts
+++ src/services/phases/squidrouter/process.ts
@@ -1,117 +1,152 @@
-import { getAccount, sendTransaction, waitForTransactionReceipt, writeContract } from '@wagmi/core';
-import { SendTransactionErrorType, WriteContractErrorType } from 'viem';
 import * as Sentry from '@sentry/react';
+import { Config, getAccount, sendTransaction, writeContract } from '@wagmi/core';
+import { Hash, SendTransactionErrorType, WriteContractErrorType } from 'viem';
 
+import { storageKeys } from '../../../constants/localStorage';
+import { EvmInputTokenDetails, getInputTokenDetails, isEvmInputTokenDetails } from '../../../constants/tokenConfig';
+
+import { waitForTransactionConfirmation } from '../../../hooks/safe-wallet/waitForTransactionConfirmation';
 import { showToast, ToastMessage } from '../../../helpers/notifications';
-import { getInputTokenDetails, isEvmInputTokenDetails } from '../../../constants/tokenConfig';
+import { TrackableEvent } from '../../../contexts/events';
 import erc20ABI from '../../../contracts/ERC20';
+import { storageService } from '../../../services/storage/local';
 
 import { ExecutionContext, OfframpingState } from '../../offrampingFlow';
 import { getRouteTransactionRequest } from './route';
+
+type TrackEvent = (event: TrackableEvent) => void;
+type TransactionRequest = { target: string; data: Hash; value: bigint; gasLimit: string };
+
+async function handleTokenApproval(
+  inputToken: EvmInputTokenDetails,
+  transactionRequest: TransactionRequest,
+  state: OfframpingState,
+  wagmiConfig: Config,
+  trackEvent: TrackEvent,
+): Promise<Hash> {
+  let approvalHash = storageService.get(storageKeys.SQUIDROUTER_RECOVERY_STATE_APPROVAL) as Hash;
+
+  if (!approvalHash) {
+    try {
+      trackEvent({ event: 'signing_requested', index: 1 });
+
+      approvalHash = await writeContract(wagmiConfig, {
+        abi: erc20ABI,
+        address: inputToken.erc20AddressSourceChain,
+        functionName: 'approve',
+        args: [transactionRequest?.target, state.inputAmount.raw],
+      });
+
+      storageService.set(storageKeys.SQUIDROUTER_RECOVERY_STATE_APPROVAL, approvalHash);
+
+      trackEvent({ event: 'transaction_signed', index: 1 });
+    } catch (e) {
+      const error = e as WriteContractErrorType;
+      handleError(error, 'writeContract', 'Failed to sign transaction');
+      throw error;
+    }
+  }
+
+  return approvalHash;
+}
+
+async function handleSwapTransaction(
+  transactionRequest: TransactionRequest,
+  wagmiConfig: Config,
+  trackEvent: TrackEvent,
+): Promise<Hash> {
+  let swapHash = storageService.get(storageKeys.SQUIDROUTER_RECOVERY_STATE_SWAP) as Hash;
+
+  if (!swapHash) {
+    try {
+      trackEvent({ event: 'signing_requested', index: 2 });
+      swapHash = await sendTransaction(wagmiConfig, {
+        to: transactionRequest.target,
+        data: transactionRequest.data,
+        value: transactionRequest.value,
+        gas: BigInt(transactionRequest.gasLimit) * BigInt(2),
+      });
+
+      storageService.set(storageKeys.SQUIDROUTER_RECOVERY_STATE_SWAP, swapHash);
+
+      trackEvent({ event: 'transaction_signed', index: 2 });
+    } catch (e) {
+      const error = e as SendTransactionErrorType;
+      handleError(error, 'sendTransaction', 'Failed to sign transaction');
+      throw error;
+    }
+  }
+
+  return swapHash;
+}
+
+function handleError(error: Error, operation: string, message: string) {
+  const errorMessage = 'shortMessage' in error ? error.shortMessage : error.message;
+  showToast(ToastMessage.SIGNING_FAILED, `${message}: ${errorMessage}`);
+
+  Sentry.captureException(error, {
+    level: 'warning',
+    extra: {
+      message: `Call to ${operation}() failed in squidRouter()`,
+      cause: 'cause' in error ? error.cause : undefined,
+    },
+  });
+
+  console.error('Error in squidRouter: ', error);
+}
 
 export async function squidRouter(
   state: OfframpingState,
   { wagmiConfig, setOfframpSigningPhase, trackEvent }: ExecutionContext,
 ): Promise<OfframpingState> {
-  const inputToken = getInputTokenDetails(state.network, state.inputTokenType);
-  if (!inputToken || !isEvmInputTokenDetails(inputToken)) {
-    throw new Error('Invalid input token type for squidRouter. Expected EVM token.');
-  }
-
-  const accountData = getAccount(wagmiConfig);
-  if (accountData?.address === undefined) {
-    throw new Error('Wallet not connected');
-  }
-
-  const { transactionRequest } = await getRouteTransactionRequest(
-    accountData.address,
-    state.inputAmount.raw,
-    state.squidRouterReceiverHash,
-    inputToken,
-    state.network,
-  );
-
-  console.log(
-    'Asking for approval of',
-    transactionRequest?.target,
-    inputToken.erc20AddressSourceChain,
-    state.inputAmount.units,
-  );
-
-  setOfframpSigningPhase?.('started');
-
-  let approvalHash;
   try {
-    trackEvent({ event: 'signing_requested', index: 1 });
-    approvalHash = await writeContract(wagmiConfig, {
-      abi: erc20ABI,
-      address: inputToken.erc20AddressSourceChain,
-      functionName: 'approve',
-      args: [transactionRequest?.target, state.inputAmount.raw],
-    });
-    trackEvent({ event: 'transaction_signed', index: 1 });
-  } catch (e) {
-    const error = e as WriteContractErrorType;
+    const inputToken = getInputTokenDetails(state.network, state.inputTokenType);
+    if (!inputToken || !isEvmInputTokenDetails(inputToken)) {
+      throw new Error('Invalid input token type for squidRouter. Expected EVM token.');
+    }
 
-    const message = 'shortMessage' in error ? error.shortMessage : error.message;
+    const accountData = getAccount(wagmiConfig);
+    if (!accountData || !accountData.address) {
+      throw new Error('Wallet not connected');
+    }
 
-    showToast(ToastMessage.SIGNING_FAILED, 'Failed to sign transaction: ' + message);
+    const { transactionRequest } = await getRouteTransactionRequest(
+      accountData.address,
+      state.inputAmount.raw,
+      state.squidRouterReceiverHash,
+      inputToken,
+      state.network,
+    );
 
-    Sentry.captureException(error, {
-      level: 'warning',
-      extra: {
-        message: 'Call to writeContract() failed in squidRouter()',
-        cause: 'cause' in error ? error.cause : undefined,
-      },
-    });
+    setOfframpSigningPhase?.('started');
 
-    console.error('Error in squidRouter: ', e);
-    return { ...state, failure: { type: 'unrecoverable', message: e?.toString() } };
+    const approvalHash = await handleTokenApproval(inputToken, transactionRequest, state, wagmiConfig, trackEvent);
+
+    await waitForTransactionConfirmation(approvalHash);
+
+    setOfframpSigningPhase?.('approved');
+
+    const swapHash = await handleSwapTransaction(transactionRequest, wagmiConfig, trackEvent);
+
+    await waitForTransactionConfirmation(swapHash);
+
+    setOfframpSigningPhase?.('signed');
+
+    const axelarScanLink = 'https://axelarscan.io/gmp/' + swapHash;
+    console.log(`Squidrouter Swap Initiated! Check Axelarscan for details: ${axelarScanLink}`);
+
+    setOfframpSigningPhase?.('finished');
+
+    storageService.remove(storageKeys.SQUIDROUTER_RECOVERY_STATE_APPROVAL);
+    storageService.remove(storageKeys.SQUIDROUTER_RECOVERY_STATE_SWAP);
+
+    return {
+      ...state,
+      squidRouterApproveHash: approvalHash as Hash,
+      squidRouterSwapHash: swapHash as Hash,
+      phase: 'pendulumFundEphemeral',
+    };
+  } catch (error) {
+    return { ...state, failure: { type: 'unrecoverable', message: error?.toString() } };
   }
-
-  setOfframpSigningPhase?.('approved');
-
-  await waitForTransactionReceipt(wagmiConfig, { hash: approvalHash });
-
-  let swapHash;
-  try {
-    trackEvent({ event: 'signing_requested', index: 2 });
-    swapHash = await sendTransaction(wagmiConfig, {
-      to: transactionRequest.target,
-      data: transactionRequest.data,
-      value: transactionRequest.value,
-      gas: BigInt(transactionRequest.gasLimit) * BigInt(2),
-    });
-    trackEvent({ event: 'transaction_signed', index: 2 });
-  } catch (e) {
-    const error = e as SendTransactionErrorType;
-
-    const message = 'shortMessage' in error ? error.shortMessage : error.message;
-
-    showToast(ToastMessage.SIGNING_FAILED, 'Failed to sign transaction: ' + message);
-    Sentry.captureException(error, {
-      level: 'warning',
-      extra: {
-        message: 'Call to sendTransaction() failed in squidRouter()',
-        cause: 'cause' in error ? error.cause : undefined,
-      },
-    });
-
-    console.error('Error in squidRouter: ', e);
-    return { ...state, failure: { type: 'unrecoverable', message: e?.toString() } };
-  }
-
-  setOfframpSigningPhase?.('signed');
-
-  const axelarScanLink = 'https://axelarscan.io/gmp/' + swapHash;
-  console.log(`Squidrouter Swap Initiated! Check Axelarscan for details: ${axelarScanLink}`);
-
-  setOfframpSigningPhase?.('finished');
-
-  return {
-    ...state,
-    squidRouterApproveHash: approvalHash,
-    squidRouterSwapHash: swapHash,
-    phase: 'pendulumFundEphemeral',
-  };
 }
