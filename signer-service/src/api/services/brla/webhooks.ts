a2404ccebe362b09ba896868043ce599c5531c24
Gianfranco <g.tasteri@gmail.com>
2025-02-26 10:22:54 -0300

    comments and readability of poll events function

--- signer-service/src/api/services/brla/webhooks.ts
+++ signer-service/src/api/services/brla/webhooks.ts
@@ -22,7 +22,11 @@ export class EventPoller {
   private started: boolean = false;
 
   constructor(pollingInterval: number = 1000) {
-    this.apiUrl = WEBHOOKS_CACHE_URL!;
+    if (!WEBHOOKS_CACHE_URL) {
+      throw new Error('WEBHOOKS_CACHE_URL is not defined!');
+    }
+
+    this.apiUrl = WEBHOOKS_CACHE_URL;
     this.pollingInterval = pollingInterval;
   }
 
@@ -39,22 +43,30 @@ export class EventPoller {
     }, this.pollingInterval);
   }
 
+  private groupEventsByUser(events: Event[]): Map<string, Event[]> {
+    return events.reduce((acc: Map<string, Event[]>, event: Event) => {
+      if (!acc.has(event.userId)) {
+        acc.set(event.userId, []);
+      }
+      acc.get(event.userId)!.push(event);
+      return acc;
+    }, new Map<string, Event[]>());
+  }
+
   private async poll() {
     try {
       const response = await fetch(this.apiUrl);
       const events: Event[] = await response.json();
 
-      // Group received events by user, then append only new events to the cache
-      const groupedEvents = events.reduce((acc: Map<string, Event[]>, event: Event) => {
-        if (!acc.has(event.userId)) {
-          acc.set(event.userId, []);
-        }
-        acc.get(event.userId)!.push(event);
-        return acc;
-      }, new Map<string, Event[]>());
+      const groupedEvents = this.groupEventsByUser(events);
 
+      // For each user, appends all new events that are not registered in the cache,
+      // by checking the last timestamp of each user map.
       groupedEvents.forEach((fetchedUserEvents, userId) => {
+        // Get the events from the cache or create an empty array
         const userEvents = this.cache.get(userId) || [];
+
+        // If there are new events, append those. Otherwise, initialize the cache with the fetched events.
         if (userEvents.length > 0) {
           const lastTimestamp = userEvents[userEvents.length - 1].createdAt;
           const eventsNotRegistered = fetchedUserEvents.filter(
