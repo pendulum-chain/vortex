f8cc120e77903572a94944ff491dcf63aa99ec94
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-02-27 18:17:33 +0100

    fix Field registers react-hook-form

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -5,9 +5,10 @@ export interface FieldProps extends InputHTMLAttributes<HTMLInputElement> {
   register?: UseFormRegisterReturn;
 }
 
-export const Field = ({ className, ...rest }: FieldProps) => (
+export const Field = ({ className, register, ...rest }: FieldProps) => (
   <input
     className={`input-vortex-primary border-1 border-neutral-300 input-ghost w-full p-2 rounded-lg ${className || ''}`}
+    {...register}
     {...rest}
   />
 );
