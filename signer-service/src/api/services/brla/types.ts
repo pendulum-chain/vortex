83acea9074515180183432a588e625b8cfcb61c5
Marcel Ebert <mail@marcel-ebert.de>
2025-03-10 19:42:06 +0100

    Change type of birthdate to string on payload

--- signer-service/src/api/services/brla/types.ts
+++ signer-service/src/api/services/brla/types.ts
@@ -48,7 +48,7 @@ export interface RegisterSubaccountPayload {
   address: BrlaAddress;
   fullName: string;
   cpf: string;
-  birthdate: number;
+  birthdate: string;
   companyName?: string;
   startDate?: string;
   cnpj?: string;
