7b60e66548d0d6da2c62b43b2a312934acb59fc0
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-03-11 17:18:58 +0100

    remove showCompareFees

--- src/pages/swap/useSwapUrlParams.ts
+++ src/pages/swap/useSwapUrlParams.ts
@@ -5,8 +5,7 @@ import { FeeComparisonRef } from '../../components/FeeComparison';
 import { OutputTokenType } from '../../constants/tokenConfig';
 
 interface UseSwapUrlParamsProps {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  form: UseFormReturn<SwapFormValues, any, undefined>;
+  form: UseFormReturn<SwapFormValues, unknown, undefined>;
   feeComparisonRef: React.RefObject<FeeComparisonRef | null>;
 }
 const defaultFromAmounts: Record<OutputTokenType, number> = { eurc: 1000, ars: 200, brl: 300 };
@@ -24,11 +23,7 @@ export const useSwapUrlParams = ({ form, feeComparisonRef }: UseSwapUrlParamsPro
       }
     }
 
-    const showCompareFeesParam = params.get('showCompareFees');
-    if (showCompareFeesParam === 'true') {
-      feeComparisonRef.current?.scrollIntoView();
-      // toToken should always exist due to hardcoded default values. Defensive.
-    } else if (toTokenForm) {
+    if (toTokenForm) {
       const defaultAmount = defaultFromAmounts[toTokenForm];
       form.setValue('fromAmount', defaultAmount.toFixed(2));
     }
