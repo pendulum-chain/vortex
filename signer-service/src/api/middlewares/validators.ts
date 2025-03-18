d9b227eb4e6f6d36a6592dac27d77ae7345ebb01
Marcel Ebert <mail@marcel-ebert.de>
2025-03-18 16:57:15 +0100

    Fix storage data headers

--- signer-service/src/api/middlewares/validators.ts
+++ signer-service/src/api/middlewares/validators.ts
@@ -3,9 +3,9 @@ import { isStellarTokenConfig, TOKEN_CONFIG, TokenConfig } from '../../constants
 import { EMAIL_SHEET_HEADER_VALUES } from '../controllers/email.controller';
 import { RATING_SHEET_HEADER_VALUES } from '../controllers/rating.controller';
 import {
-  DUMP_SHEET_HEADER_VALUES_ASSETHUB,
-  DUMP_SHEET_HEADER_VALUES_EVM_MOONBEAM,
-  DUMP_SHEET_HEADER_VALUES_EVM_STELLAR,
+  DUMP_SHEET_HEADER_VALUES_ASSETHUB_TO_STELLAR,
+  DUMP_SHEET_HEADER_VALUES_EVM_TO_BRLA,
+  DUMP_SHEET_HEADER_VALUES_EVM_TO_STELLAR,
 } from '../controllers/storage.controller';
 import {
   SUPPORTED_PROVIDERS,
@@ -159,9 +159,11 @@ const validateRequestBodyValuesForTransactionStore = (): RequestHandler => (req,
 
   const requiredRequestBodyKeys = offramperAddress.includes('0x')
     ? req.body.stellarEphemeralPublicKey
-      ? DUMP_SHEET_HEADER_VALUES_EVM_STELLAR
-      : DUMP_SHEET_HEADER_VALUES_EVM_MOONBEAM
-    : DUMP_SHEET_HEADER_VALUES_ASSETHUB;
+      ? DUMP_SHEET_HEADER_VALUES_EVM_TO_STELLAR
+      : DUMP_SHEET_HEADER_VALUES_EVM_TO_BRLA
+    : req.body.stellarEphemeralPublicKey
+    ? DUMP_SHEET_HEADER_VALUES_ASSETHUB_TO_STELLAR
+    : DUMP_SHEET_HEADER_VALUES_EVM_TO_BRLA;
 
   validateRequestBodyValues(requiredRequestBodyKeys)(req, res, next);
 };
