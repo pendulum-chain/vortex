6fb8eb5cea8305bf48547b15abb475fcef33bc3c
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-03-26 10:01:56 +0100

    cn Field

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -1,5 +1,6 @@
 import { InputHTMLAttributes } from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
+import { cn } from '../../helpers/cn';
 
 export interface FieldProps extends InputHTMLAttributes<HTMLInputElement> {
   register?: UseFormRegisterReturn;
@@ -7,7 +8,7 @@ export interface FieldProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export const Field = ({ className, register, ...rest }: FieldProps) => (
   <input
-    className={`input-vortex-primary border-1 border-neutral-300 input-ghost w-full p-2 rounded-lg ${className || ''}`}
+    className={cn('input-vortex-primary border-1 border-neutral-300 input-ghost w-full p-2 rounded-lg', className)}
     {...register}
     {...rest}
   />
