2d4121a965fbc16355a1c650a6f4b8858c13cb5f
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-03-26 10:08:05 +0100

    cn PolkadotConnectWallet

--- src/components/buttons/PolkadotWalletButton/PolkadotConnectWallet/index.tsx
+++ src/components/buttons/PolkadotWalletButton/PolkadotConnectWallet/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { PlayCircleIcon } from '@heroicons/react/20/solid';
 import { PolkadotWalletSelectorDialog } from '../../../PolkadotWalletSelectorDialog';
 import { useTranslation } from 'react-i18next';
+import { cn } from '../../../../helpers/cn';
 
 export const PolkadotConnectWallet = ({ customStyles, hideIcon }: { customStyles?: string; hideIcon?: boolean }) => {
   const [showPolkadotDialog, setShowPolkadotDialog] = useState(false);
@@ -14,7 +15,7 @@ export const PolkadotConnectWallet = ({ customStyles, hideIcon }: { customStyles
           setShowPolkadotDialog(true);
         }}
         type="button"
-        className={`btn rounded-3xl group ${customStyles || 'btn-vortex-secondary'}`}
+        className={cn('btn rounded-3xl group', customStyles || 'btn-vortex-secondary')}
       >
         <p className="flex">
           {t('components.dialogs.connectWallet.connect')} <span className="hidden lg:block lg:ml-1">Wallet</span>
