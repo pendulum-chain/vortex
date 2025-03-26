79ad639f1f7152e432d00fe64438372d86937e6c
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-03-26 10:06:12 +0100

    cn RatingForm

--- src/components/Rating/RatingForm/index.tsx
+++ src/components/Rating/RatingForm/index.tsx
@@ -1,5 +1,6 @@
 import { motion, AnimatePresence } from 'motion/react';
 import { useTranslation } from 'react-i18next';
+import { cn } from '../../../helpers/cn';
 
 // From the highest to the lowest rating - because of ".rating input:hover ~ input" in ./index.css
 // https://developer.mozilla.org/en-US/docs/Web/CSS/Subsequent-sibling_combinator
@@ -20,12 +21,12 @@ export const RatingForm: React.FC<RatingFormProps> = ({ onSubmit, isFormSubmitte
       <h2 className="w-full text-center text-md sm:text-lg">
         {isFormSubmitted ? t('rating.thankYou') : t('rating.howWouldYouRateYourExperience')}
       </h2>
-      <form className={`rating rating-lg mt-2.5 pb-5 flex flex-row-reverse ${isFormSubmitted ? 'rating-checked' : ''}`}>
+      <form className={cn('rating rating-lg mt-2.5 pb-5 flex flex-row-reverse', isFormSubmitted && 'rating-checked')}>
         <AnimatePresence>
           <input type="radio" defaultChecked name="rating" className="hidden" />
           {ratings.filter(filterRatingsBasedOnUserInput).map((ratingValue, index) => (
             <motion.input
-              className={`transition bg-orange-400 mask mask-star-2 ${index !== 0 ? 'mr-2.5' : ''}`}
+              className={cn('transition bg-orange-400 mask mask-star-2', index !== 0 && 'mr-2.5')}
               value={ratingValue}
               key={ratingValue}
               type="radio"
