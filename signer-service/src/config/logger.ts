2ca572f4967f8ebe1c79a00c48240092b83982ae
Marcel Ebert <mail@marcel-ebert.de>
2025-03-27 19:06:47 +0100

    Fix logging of objects

--- signer-service/src/config/logger.ts
+++ signer-service/src/config/logger.ts
@@ -1,22 +1,26 @@
-import { StreamOptions } from 'morgan';
-import winston from 'winston';
+import { StreamOptions } from "morgan";
+import winston, { format } from "winston";
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message, ...meta }) => {
-      const metaString = meta && Object.keys(meta).length ? JSON.stringify(meta, null, 2) : '';
-      return `${timestamp} [${level}]: ${message} ${metaString}`;
-    }),
+  level: "info",
+  format: format.combine(
+    format.prettyPrint(),
+    format.splat(),
+    format.printf((info) => {
+      if (typeof info.message === "object") {
+        info.message = JSON.stringify(info.message, null, 3);
+      }
+
+      return info.message;
+    })
   ),
   transports: [
     //
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
   ],
 });
 
@@ -24,17 +28,11 @@ const logger = winston.createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.printf(({ timestamp, level, message, ...meta }) => {
-          const metaString = meta && Object.keys(meta).length ? JSON.stringify(meta, null, 2) : '';
-          return `${timestamp} [${level}]: ${message} ${metaString}`;
-        }),
-      ),
-    }),
+      format: winston.format.simple(),
+    })
   );
 }
 
