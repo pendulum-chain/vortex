b696ca18dc687498dfef760848cc58ad4960a319
Marcel Ebert <mail@marcel-ebert.de>
2025-04-07 17:11:14 +0200

    Always log to console, no matter the NODE_ENV

--- api/src/config/logger.ts
+++ api/src/config/logger.ts
@@ -21,20 +21,12 @@ const logger = winston.createLogger({
     //
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
+    // This console transport is always active, regardless of environment
     new winston.transports.Console({
       format: winston.format.simple(),
     }),
-  );
-}
+  ],
+});
 
 const stream: StreamOptions = {
   write: (message: string) => {
