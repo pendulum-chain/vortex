b4e1b525436bb0f44c2b8931be137699c8de0505
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-04-08 07:55:41 +0200

    RampFeeCollapse handles Onramp

--- frontend/src/components/RampFeeCollapse/index.tsx
+++ frontend/src/components/RampFeeCollapse/index.tsx
@@ -4,12 +4,15 @@ import Big from 'big.js';
 import { FeeCollapse } from '../FeeCollapse';
 import { ExchangeRate } from '../ExchangeRate';
 import { useOnChainToken, useInputAmount, useFiatToken } from '../../stores/ramp/useRampFormStore';
-import { useQuoteService } from '../../hooks/ramp/useQuoteService';
 import { useNetwork } from '../../contexts/network';
 import { getOnChainTokenDetailsOrDefault, getAnyFiatTokenDetails } from 'shared';
+import { useQuoteOutputAmount, useQuoteExchangeRate } from '../../stores/ramp/useQuoteStore';
+import { useRampDirection } from '../../stores/rampDirectionStore';
+import { RampDirection } from '../RampToggle';
 
-export const RampFeeCollapse: FC = () => {
+export const RampFeeCollapse = () => {
   const { selectedNetwork } = useNetwork();
+  const rampDirection = useRampDirection();
 
   const from = useOnChainToken();
   const to = useFiatToken();
@@ -17,14 +20,16 @@ export const RampFeeCollapse: FC = () => {
 
   const fromToken = getOnChainTokenDetailsOrDefault(selectedNetwork, from);
   const toToken = getAnyFiatTokenDetails(to);
+  const toAmount = useQuoteOutputAmount();
+  const exchangeRate = useQuoteExchangeRate();
 
-  const { outputAmount: toAmount, exchangeRate } = useQuoteService(fromAmount, from, to);
+  const displayToken = rampDirection === RampDirection.ONRAMP ? fromToken : fromToken;
 
   return (
     <FeeCollapse
       fromAmount={fromAmount?.toString()}
       toAmount={toAmount || Big(0)}
-      toToken={toToken}
+      toToken={displayToken}
       exchangeRate={
         <ExchangeRate exchangeRate={exchangeRate} fromToken={fromToken} toTokenSymbol={toToken.fiat.symbol} />
       }
