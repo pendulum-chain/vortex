61d40f88fe7cdda6bb9348c3cf5713214fc3bc6e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-04-08 15:55:22 +0200

    fix UserBalance

--- frontend/src/components/UserBalance/index.tsx
+++ frontend/src/components/UserBalance/index.tsx
@@ -6,33 +6,51 @@ import wallet from '../../assets/wallet-bifold-outline.svg';
 interface UserBalanceProps {
   token: OnChainTokenDetails;
   onClick: (amount: string) => void;
+  className?: string;
 }
 
-export const UserBalance = ({ token, onClick }: UserBalanceProps) => {
-  const { isDisconnected } = useVortexAccount();
-  const onchainTokenBalance = useOnchainTokenBalance({ token });
+const SimpleBalance = ({ token, className }: { token: OnChainTokenDetails; className?: string }) => {
+  const onchainTokenBalanceRaw = useOnchainTokenBalance({ token });
+  const onchainTokenBalance = onchainTokenBalanceRaw?.balance || '0';
 
-  if (isDisconnected || onchainTokenBalance === undefined) {
-    return <div className="h-6 mt-1" />;
-  }
+  return (
+    <p className={className}>
+      {onchainTokenBalance} {token.assetSymbol}
+    </p>
+  );
+};
 
-  const showMaxButton = Number(onchainTokenBalance) !== 0;
+const FullBalance = ({ token, onClick }: { token: OnChainTokenDetails; onClick: (amount: string) => void }) => {
+  const onchainTokenBalanceRaw = useOnchainTokenBalance({ token });
+  const onchainTokenBalance = onchainTokenBalanceRaw?.balance || '0';
 
+  const hasBalance = onchainTokenBalance !== undefined && Number(onchainTokenBalance) !== 0;
+
+  if (!hasBalance) return null;
   return (
     <div className="flex items-center justify-end mt-1 mr-0.5">
       <img src={wallet} alt="Available" className="w-5 h-5 mr-0.5" />
       <p>
-        {onchainTokenBalance.balance} {token.assetSymbol}
+        {onchainTokenBalance} {token.assetSymbol}
       </p>
-      {showMaxButton && (
-        <button
-          className="px-1 ml-1 bg-blue-100 rounded-md text-primary hover:underline"
-          type="button"
-          onClick={() => onClick(onchainTokenBalance.balance)}
-        >
-          Max
-        </button>
-      )}
+      <button
+        className="px-1 ml-1 bg-blue-100 rounded-md text-primary hover:underline"
+        type="button"
+        onClick={() => onClick(onchainTokenBalance)}
+      >
+        Max
+      </button>
     </div>
+  );
+};
+
+export const UserBalance = ({ token, onClick, className }: UserBalanceProps) => {
+  const { isDisconnected } = useVortexAccount();
+
+  if (isDisconnected) return null;
+  return onClick ? (
+    <FullBalance token={token} onClick={onClick} />
+  ) : (
+    <SimpleBalance token={token} className={className} />
   );
 };
