a1fd6fef2760875c639a7b0bec0043cce9937623
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-04-10 11:46:57 +0200

    add useRampService (register,start)

---
+++ frontend/src/hooks/offramp/useRampService/brl/useStartRampBRL.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useRampStore } from '../../../../stores/offrampStore';
+import { RampService } from '../../../../services/api';
+
+export const useStartRamp = () => {
+  const {
+    rampState,
+    rampStarted,
+    actions: { setRampStarted },
+  } = useRampStore();
+
+  useEffect(() => {
+    const shouldStartRamp =
+      rampState?.userSigningMeta && !rampStarted && rampState?.ramp && (rampState?.signedTransactions?.length || 0) > 0;
+
+    if (!shouldStartRamp) {
+      return;
+    }
+
+    console.log('Proceeding to start the ramp...');
+
+    console.log('startRamp');
+    RampService.startRamp(rampState.quote.id, rampState.signedTransactions, rampState.userSigningMeta)
+      .then((response) => {
+        console.log('startRampResponse', response);
+        setRampStarted(true);
+      })
+      .catch((err) => {
+        console.error('Error starting ramp:', err);
+      });
+  }, [rampStarted, rampState, setRampStarted]);
+};
