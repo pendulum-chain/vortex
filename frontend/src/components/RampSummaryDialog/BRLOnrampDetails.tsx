d5c004cbfd8acac5dcb2c66c2a6b062271eda8bd
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-04-15 14:12:06 +0200

    fix translations BRLOnrampDetails

--- frontend/src/components/RampSummaryDialog/BRLOnrampDetails.tsx
+++ frontend/src/components/RampSummaryDialog/BRLOnrampDetails.tsx
@@ -17,17 +17,17 @@ export const BRLOnrampDetails: FC = () => {
   return (
     <section>
       <hr className="my-5" />
-      <h1 className="font-bold text-lg">{t('components.dialogs.OfframpSummaryDialog.BRLOnrampDetails.title')}</h1>
+      <h1 className="font-bold text-lg">{t('components.dialogs.RampSummaryDialog.BRLOnrampDetails.title')}</h1>
       <h2 className="font-bold text-center text-lg">
-        {t('components.dialogs.OfframpSummaryDialog.BRLOnrampDetails.description')}
+        {t('components.dialogs.RampSummaryDialog.BRLOnrampDetails.description')}
       </h2>
-      <p className="pt-2 text-center">{t('components.dialogs.OfframpSummaryDialog.BRLOnrampDetails.qrCode')}</p>
+      <p className="pt-2 text-center">{t('components.dialogs.RampSummaryDialog.BRLOnrampDetails.qrCode')}</p>
       <div className="flex justify-center my-6">
         <div className="border-1 border-gray-300 rounded-lg p-4">
           <QRCodeSVG value={rampState.ramp?.brCode} />
         </div>
       </div>
-      <p className="text-center">{t('components.dialogs.OfframpSummaryDialog.BRLOnrampDetails.copyCode')}</p>
+      <p className="text-center">{t('components.dialogs.RampSummaryDialog.BRLOnrampDetails.copyCode')}</p>
       <CopyButton text={rampState.ramp?.brCode} className="w-full mt-4 py-10" />
     </section>
   );
