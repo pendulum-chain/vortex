f89c054e5c7feb8da14d463300aadd264b8d9f17
Gianfranco <g.tasteri@gmail.com>
2025-04-22 14:11:07 -0300

    improve validateMaskedNumber

--- shared/src/helpers/brla.ts
+++ shared/src/helpers/brla.ts
@@ -2,6 +2,10 @@
  * Validates a tax id (CPF) vs a partially masked tax id received from BRLA's pix-info endpoint
  */
 export function validateMaskedNumber(maskedCpf: string, fullCpf: string): boolean {
+
+  maskedCpf = maskedCpf.replace(/[-.]/g, '');
+  fullCpf = fullCpf.replace(/[-.]/g, '');
+
   if (!maskedCpf || !fullCpf) return false;
 
   if (maskedCpf.length !== fullCpf.length) throw new Error('Invalid comparison. Lengths do not match.');
