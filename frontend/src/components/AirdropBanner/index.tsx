0b0b88cfa8d3f85247aae2aedf545af0f517a63e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-05-06 16:51:36 +0200

    update AirdropBanner

--- frontend/src/components/AirdropBanner/index.tsx
+++ frontend/src/components/AirdropBanner/index.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react';
-import { motion, useMotionValue, AnimatePresence } from 'motion/react';
+import { motion, AnimatePresence } from 'motion/react';
 import { useTranslation, Trans } from 'react-i18next';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 export const AirdropBanner = () => {
   const { t } = useTranslation();
   const [expanded, setExpanded] = useState(false);
-  const rotation = useMotionValue(0);
 
   const toggleExpand = () => {
-    rotation.set(expanded ? 0 : 180);
-    setExpanded(!expanded);
+    setExpanded((state) => !state);
   };
 
   return (
@@ -20,17 +19,18 @@ export const AirdropBanner = () => {
       transition={{ duration: 0.5, ease: 'easeOut' }}
       whileHover={{ scale: 1.01 }}
     >
-      <div className='flex flex-col items-center justify-center'>
-       <div className="flex items-center justify-between text-center">
-        <div>
-       <h1 className='text-xl'>🪂<strong> {t('components.airdropBanner.title')}</strong></h1>
-        <p className="font-medium">
-            {t('components.airdropBanner.description')}
-        </p>
-       </div>
-
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex items-center justify-between text-center">
+          <div>
+            <h1 className="text-xl">
+              🪂<strong> {t('components.airdropBanner.title')}</strong>
+            </h1>
+            <p className="font-medium">{t('components.airdropBanner.description')}</p>
+          </div>
+          <div>
+            <ChevronDownIcon className={`w-6 h-6 transition-transform duration-300 ${expanded ? 'rotate-180' : ''}`} />
+          </div>
         </div>
-        <button className="bg-[#000] text-white px-4 py-2 rounded-md mt-4 cursor-pointer">{t('components.airdropBanner.button')}</button>
       </div>
 
       <AnimatePresence>
@@ -57,7 +57,10 @@ export const AirdropBanner = () => {
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ delay: 0.2 }}
               >
-                <Trans i18nKey="components.airdropBanner.list.2">All eligible users get $10 USDT - <strong>the first 100 receive a 10 bonus, totaling 20 USDT.</strong> Rewards go to your wallet on Base or AssetHub.</Trans>
+                <Trans i18nKey="components.airdropBanner.list.2">
+                  All eligible users get $10 USDT - <strong>the first 100 receive a 10 bonus, totaling 20 USDT.</strong>{' '}
+                  Rewards go to your wallet on Base or AssetHub.
+                </Trans>
               </motion.li>
               <motion.li
                 className="mb-2"
@@ -65,7 +68,19 @@ export const AirdropBanner = () => {
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ delay: 0.3 }}
               >
-                {t('components.airdropBanner.list.3')}
+                <Trans i18nKey="components.airdropBanner.list.3">
+                  One reward per person -
+                  <a
+                    className="underline hover:text-blue-700 transition"
+                    href="https://github.com/pendulum-chain/vortex/blob/campaign-may-2025/eligible-users.csv"
+                    target="_blank"
+                    onClick={(e) => e.stopPropagation()}
+                    rel="noreferrer"
+                  >
+                    Check here
+                  </a>{' '}
+                  (Updates every 24hrs)
+                </Trans>
               </motion.li>
             </ul>
           </motion.div>
