7c08a9d9e50bd2a7b9aec326c4d4f87aa487159e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-05-15 13:51:49 +0200

    create SigningBox button

--- frontend/src/components/SigningBox/SigningBoxContent.tsx
+++ frontend/src/components/SigningBox/SigningBoxContent.tsx
@@ -3,33 +3,36 @@ import { useTranslation } from 'react-i18next';
 import accountBalanceWalletIcon from '../../assets/account-balance-wallet-blue.svg';
 import { Spinner } from '../Spinner';
 
-interface SigningBoxContentProps {
-  progress: number;
+interface SigningBoxButtonProps {
   signatureState: { max: number; current: number };
   confirmations: { required: number; current: number };
-  className?: string;
-  showHeader?: boolean;
-  showFooter?: boolean;
 }
 
-export const SigningBoxContent: React.FC<SigningBoxContentProps> = ({
-  progress,
-  signatureState,
-  confirmations,
-  className = '',
-  showHeader = true,
-  showFooter = true,
-}) => {
+export const SigningBoxButton = ({ signatureState, confirmations }: SigningBoxButtonProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <button className="btn-vortex-primary btn rounded-xl" disabled style={{ flex: '1 1 calc(50% - 0.75rem/2)' }}>
+      <Spinner />
+      <p className="ml-2.5 my-2 text-xs">
+        {t('components.signingBox.waitingForSignature')} {signatureState.current}/{signatureState.max}
+        {confirmations.required > 0
+          ? `. (${t('components.signingBox.signatures')} ${confirmations.current}/${confirmations.required})`
+          : ''}
+      </p>
+    </button>
+  );
+};
+interface SigningBoxContentProps {
+  progress: number;
+  className?: string;
+}
+
+export const SigningBoxContent: React.FC<SigningBoxContentProps> = ({ progress, className = '' }) => {
   const { t } = useTranslation();
 
   return (
     <div className={className}>
-      {showHeader && (
-        <motion.header className="bg-pink-500 rounded-t">
-          <h1 className="w-full py-2 text-center text-white">{t('components.signingBox.actionRequired')}</h1>
-        </motion.header>
-      )}
-
       <main className="px-8 bg-white">
         <motion.div className="flex items-center justify-center">
           <div className="flex items-center justify-center w-10 h-10 border rounded-full border-primary">
@@ -52,18 +55,6 @@ export const SigningBoxContent: React.FC<SigningBoxContentProps> = ({
           </div>
         </motion.div>
       </main>
-
-      {showFooter && (
-        <motion.footer className="flex items-center justify-center bg-[#5E88D5] text-white rounded-b">
-          <Spinner />
-          <p className="ml-2.5 my-2 text-xs">
-            {t('components.signingBox.waitingForSignature')} {signatureState.current}/{signatureState.max}
-            {confirmations.required > 0
-              ? `. (${t('components.signingBox.signatures')} ${confirmations.current}/${confirmations.required})`
-              : ''}
-          </p>
-        </motion.footer>
-      )}
     </div>
   );
 };
