946e1a14748a2d1c81702f5bb8aaaf0c56905bc7
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-05-16 21:09:03 +0200

    disable BSC on UI

--- frontend/src/wagmiConfig.ts
+++ frontend/src/wagmiConfig.ts
@@ -1,4 +1,4 @@
-import { polygon, bsc, arbitrum, base, avalanche, mainnet } from '@reown/appkit/networks';
+import { polygon, arbitrum, base, avalanche, mainnet } from '@reown/appkit/networks';
 import { http } from 'wagmi';
 import { config } from './config';
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
@@ -9,7 +9,7 @@ const transports = config.alchemyApiKey
   ? {
       [polygon.id]: http(`https://polygon-mainnet.g.alchemy.com/v2/${config.alchemyApiKey}`),
       [mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${config.alchemyApiKey}`),
-      [bsc.id]: http(`https://bnb-mainnet.g.alchemy.com/v2/${config.alchemyApiKey}`),
+      // [bsc.id]: http(`https://bnb-mainnet.g.alchemy.com/v2/${config.alchemyApiKey}`),
       [arbitrum.id]: http(`https://arb-mainnet.g.alchemy.com/v2/${config.alchemyApiKey}`),
       [base.id]: http(`https://base-mainnet.g.alchemy.com/v2/${config.alchemyApiKey}`),
       [avalanche.id]: http(`https://avax-mainnet.g.alchemy.com/v2/${config.alchemyApiKey}`),
@@ -17,7 +17,7 @@ const transports = config.alchemyApiKey
   : {
       [polygon.id]: http(''),
       [mainnet.id]: http(''),
-      [bsc.id]: http(''),
+      // [bsc.id]: http(''),
       [arbitrum.id]: http(''),
       [base.id]: http(''),
       [avalanche.id]: http(''),
@@ -32,7 +32,7 @@ const metadata = {
 };
 
 // 3. Set the networks
-const networks = [mainnet, polygon, bsc, arbitrum, base, avalanche];
+const networks = [mainnet, polygon, arbitrum, base, avalanche];
 
 const projectId = '495a5f574d57e27fd65caa26d9ea4f10';
 // 4. Create Wagmi Adapter
