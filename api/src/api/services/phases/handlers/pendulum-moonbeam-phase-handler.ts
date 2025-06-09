82684ea834d7f649a8574066078a3fa7ee77195e
Gianfranco <g.tasteri@gmail.com>
2025-06-09 15:42:10 -0300

    add pre execution check for pendulum to moonbeam phase

--- api/src/api/services/phases/handlers/pendulum-moonbeam-phase-handler.ts
+++ api/src/api/services/phases/handlers/pendulum-moonbeam-phase-handler.ts
@@ -1,11 +1,14 @@
-import { RampPhase, decodeSubmittableExtrinsic, getAddressForFormat } from 'shared';
+import { AXL_USDC_MOONBEAM, FiatToken, RampPhase, decodeSubmittableExtrinsic, getAddressForFormat, getAnyFiatTokenDetailsMoonbeam } from 'shared';
 import { BasePhaseHandler } from '../base-phase-handler';
 import RampState from '../../../../models/rampState.model';
 
 import { submitXTokens } from '../../xcm/send';
-
+import { moonbeam } from 'viem/chains';
 import { ApiManager } from '../../pendulum/apiManager';
 import { StateMetadata } from '../meta-state-types';
+import { getEvmTokenBalance } from '../../moonbeam/balance';
+import Big from 'big.js';
+
 
 export class PendulumToMoonbeamXCMPhaseHandler extends BasePhaseHandler {
   public getPhaseName(): RampPhase {
@@ -14,16 +17,31 @@ export class PendulumToMoonbeamXCMPhaseHandler extends BasePhaseHandler {
 
   protected async executePhase(state: RampState): Promise<RampState> {
     const apiManager = ApiManager.getInstance();
-    const networkName = 'pendulum';
-    const pendulumNode = await apiManager.getApi(networkName);
+    const pendulumNode = await apiManager.getApi('pendulum');
 
-    const { pendulumEphemeralAddress } = state.state as StateMetadata;
+    const { pendulumEphemeralAddress, moonbeamEphemeralAddress, outputAmountBeforeFinalStep, outputTokenType } = state.state as StateMetadata;
 
-    if (!pendulumEphemeralAddress) {
-      throw new Error('Pendulum ephemeral address is not defined in the state. This is a bug.');
+    if (!pendulumEphemeralAddress || !moonbeamEphemeralAddress) {
+      throw new Error('Ephemeral address(es) not defined in the state. This is a bug.');
     }
 
+    const didInputTokenArrivedOnMoonbeam = async () => {
+      // Token is always either axlUSDC or BRL.
+      const tokenAddress = state.type === 'off' ? getAnyFiatTokenDetailsMoonbeam(FiatToken.BRL).moonbeamErc20Address : AXL_USDC_MOONBEAM;
+      const balance = await getEvmTokenBalance({
+        tokenAddress: tokenAddress as `0x${string}`,
+        ownerAddress: moonbeamEphemeralAddress as `0x${string}`,
+        chain: moonbeam,
+      });
+
+      return balance.gte(Big(outputAmountBeforeFinalStep.raw));
+    };
+
     try {
+      if (await didInputTokenArrivedOnMoonbeam()) {
+          return this.transitionToNextPhase(state, this.nextPhaseSelector(state));
+      }
+
       const { txData: pendulumToMoonbeamTransaction } = this.getPresignedTransaction(state, 'pendulumToMoonbeam');
 
       if (typeof pendulumToMoonbeamTransaction !== 'string') {
