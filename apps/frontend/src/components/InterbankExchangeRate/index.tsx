82242fc1aca00f0f64f0f97130e223d8efb624cb
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-06-19 09:50:34 +0200

    implement InterbankExchangeRate

---
+++ apps/frontend/src/components/InterbankExchangeRate/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+
+interface InterbankExchangeRateProps {
+  rate: number;
+  inputCurrency: string;
+  outputCurrency: string;
+  className?: string;
+  asSpan?: boolean;
+}
+
+function formatExchangeRateString(rate: number, input: string, output: string) {
+  return `1 ${input} ≈ ${rate.toFixed(4)} ${output}`;
+}
+
+export const InterbankExchangeRate: FC<InterbankExchangeRateProps> = ({
+  rate,
+  inputCurrency,
+  outputCurrency,
+  className = "text-center text-gray-600 text-sm",
+  asSpan = false
+}) => {
+  const content = formatExchangeRateString(rate, inputCurrency, outputCurrency);
+
+  if (asSpan) {
+    return <span className={className}>{content}</span>;
+  }
+
+  return <div className={className}>{content}</div>;
+};
