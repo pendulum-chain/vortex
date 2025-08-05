b29cc909a155710a7b52b000ecb17032530a2976
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-08-05 11:04:39 +0200

    dont show token selection in a modal

--- apps/frontend/src/components/SelectionDialog/SelectionModal.tsx
+++ apps/frontend/src/components/SelectionDialog/SelectionModal.tsx
@@ -1,40 +1,12 @@
-import { FiatToken, FiatTokenDetails, Networks, OnChainToken, OnChainTokenDetails } from "@packages/shared";
-import { useTranslation } from "react-i18next";
-import { useRampModalActions, useRampModalState } from "../../stores/rampModalStore";
-import { Dialog } from "../Dialog";
+import { useRampModalState } from "../../stores/rampModalStore";
 import { Skeleton } from "../Skeleton";
 import { TokenSelectionList } from "./TokenSelectionList";
 
-export interface TokenDefinition {
-  assetSymbol: string;
-  name?: string;
-  assetIcon: string;
-  type: OnChainToken | FiatToken;
-  details: OnChainTokenDetails | FiatTokenDetails;
-}
-
-export interface ExtendedTokenDefinition extends TokenDefinition {
-  network: Networks;
-  networkDisplayName: string;
-}
-
 export function PoolSelectorModal() {
-  const { t } = useTranslation();
-
   const { isOpen, isLoading } = useRampModalState();
-
-  const { closeTokenSelectModal } = useRampModalActions();
-
   const content = isLoading ? <LoadingContent /> : <TokenSelectionList />;
 
-  return (
-    <Dialog
-      content={content}
-      headerText={t("components.dialogs.selectionModal.title")}
-      onClose={closeTokenSelectModal}
-      visible={isOpen}
-    />
-  );
+  return isOpen ? content : null;
 }
 
 function LoadingContent() {
