e76a23fc143b4a5c8ab04567326938f022ab483f
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-08-13 18:06:34 +0200

    reduce code duplication and optimize function calls in TokenSelectionList helpers

--- apps/frontend/src/components/TokenSelection/TokenSelectionList/helpers.tsx
+++ apps/frontend/src/components/TokenSelection/TokenSelectionList/helpers.tsx
@@ -46,7 +46,7 @@ export function useTokenDefinitions(filter: string, selectedNetworkFilter: Netwo
   }, [allDefinitions, selectedNetworkFilter]);
 
   const tokenDetails = useMemo(() => networkFilteredDefinitions.map(d => d.details), [networkFilteredDefinitions]);
-  const definitionsWithBalance = useOnchainTokenBalances(tokenDetails);
+  const definitionsWithBalance = useOnchainTokenBalances(tokenDetails as OnChainTokenDetails[]);
 
   const balanceMap = useMemo(() => {
     if (!definitionsWithBalance.length) return {};
@@ -106,71 +106,47 @@ function getOnChainTokensDefinitionsForNetwork(selectedNetwork: Networks): Exten
   } else throw new Error(`Network ${selectedNetwork} is not a valid origin network`);
 }
 
-function getAllSupportedTokenDefinitions(type: "from" | "to", direction: RampDirection): ExtendedTokenDefinition[] {
-  const isOnramp = direction === RampDirection.BUY;
+function getAllOnChainTokens(): ExtendedTokenDefinition[] {
   const allTokens: ExtendedTokenDefinition[] = [];
 
-  if (isOnramp) {
-    if (type === "from") {
-      // Fiat tokens (from moonbeam and stellar configs)
-      const fiatTokens = [
-        ...Object.entries(moonbeamTokenConfig),
-        ...Object.entries(stellarTokenConfig).filter(([key]) => key === FiatToken.EURC)
-      ].map(([key, value]) => ({
-        assetIcon: value.fiat.assetIcon,
-        assetSymbol: value.fiat.symbol,
-        details: value as FiatTokenDetails,
-        name: value.fiat.name,
-        network: key === FiatToken.BRL ? Networks.Moonbeam : Networks.Stellar,
-        networkDisplayName:
-          key === FiatToken.BRL ? getNetworkDisplayName(Networks.Moonbeam) : getNetworkDisplayName(Networks.Stellar),
-        type: getEnumKeyByStringValue(FiatToken, key) as FiatToken
-      }));
-      allTokens.push(...fiatTokens);
-    } else {
-      // OnChain tokens from all networks
-      // AssetHub tokens
-      allTokens.push(...getOnChainTokensDefinitionsForNetwork(Networks.AssetHub));
+  // AssetHub tokens
+  allTokens.push(...getOnChainTokensDefinitionsForNetwork(Networks.AssetHub));
 
-      // EVM network tokens
-      const evmNetworks = Object.values(Networks).filter(isNetworkEVM).filter(doesNetworkSupportRamp) as EvmNetworks[];
-      for (const network of evmNetworks) {
-        if (evmTokenConfig[network]) {
-          allTokens.push(...getOnChainTokensDefinitionsForNetwork(network));
-        }
-      }
-    }
-  } else {
-    // Offramp
-    if (type === "from") {
-      // OnChain tokens from all networks
-      // AssetHub tokens
-      allTokens.push(...getOnChainTokensDefinitionsForNetwork(Networks.AssetHub));
-
-      // EVM network tokens
-      const evmNetworks = Object.values(Networks).filter(isNetworkEVM) as EvmNetworks[];
-      for (const network of evmNetworks) {
-        if (evmTokenConfig[network]) {
-          allTokens.push(...getOnChainTokensDefinitionsForNetwork(network));
-        }
-      }
-    } else {
-      // Fiat tokens
-      const fiatTokens = [...Object.entries(moonbeamTokenConfig), ...Object.entries(stellarTokenConfig)].map(
-        ([key, value]) => ({
-          assetIcon: value.fiat.assetIcon,
-          assetSymbol: value.fiat.symbol,
-          details: value as FiatTokenDetails,
-          name: value.fiat.name,
-          network: key === FiatToken.BRL ? Networks.Moonbeam : Networks.Stellar,
-          networkDisplayName:
-            key === FiatToken.BRL ? getNetworkDisplayName(Networks.Moonbeam) : getNetworkDisplayName(Networks.Stellar),
-          type: getEnumKeyByStringValue(FiatToken, key) as FiatToken
-        })
-      );
-      allTokens.push(...fiatTokens);
+  // EVM network tokens
+  const evmNetworks = Object.values(Networks).filter(isNetworkEVM).filter(doesNetworkSupportRamp) as EvmNetworks[];
+  for (const network of evmNetworks) {
+    if (evmTokenConfig[network]) {
+      allTokens.push(...getOnChainTokensDefinitionsForNetwork(network));
     }
   }
 
   return allTokens;
+}
+
+function getFiatTokens(filterEurcOnly = false): ExtendedTokenDefinition[] {
+  const moonbeamEntries = Object.entries(moonbeamTokenConfig);
+  const stellarEntries = filterEurcOnly
+    ? Object.entries(stellarTokenConfig).filter(([key]) => key === FiatToken.EURC)
+    : Object.entries(stellarTokenConfig);
+
+  return [...moonbeamEntries, ...stellarEntries].map(([key, value]) => ({
+    assetIcon: value.fiat.assetIcon,
+    assetSymbol: value.fiat.symbol,
+    details: value as FiatTokenDetails,
+    name: value.fiat.name,
+    network: key === FiatToken.BRL ? Networks.Moonbeam : Networks.Stellar,
+    networkDisplayName:
+      key === FiatToken.BRL ? getNetworkDisplayName(Networks.Moonbeam) : getNetworkDisplayName(Networks.Stellar),
+    type: getEnumKeyByStringValue(FiatToken, key) as FiatToken
+  }));
+}
+
+function getAllSupportedTokenDefinitions(type: "from" | "to", direction: RampDirection): ExtendedTokenDefinition[] {
+  const isBuy = direction === RampDirection.BUY;
+
+  if ((isBuy && type === "from") || (!isBuy && type === "to")) {
+    return getFiatTokens(isBuy && type === "from");
+  } else {
+    return getAllOnChainTokens();
+  }
 }
