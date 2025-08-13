82148cb8be9c0303c789f92cdc094cee9539e003
Marcel Ebert <mail@marcel-ebert.de>
2025-08-13 11:45:18 +0200

    Add stepper to top of page

--- apps/frontend/src/layouts/index.tsx
+++ apps/frontend/src/layouts/index.tsx
@@ -2,6 +2,8 @@ import { FC, ReactNode, useEffect } from "react";
 import { Footer } from "../components/Footer";
 import { MaintenanceBanner } from "../components/MaintenanceBanner";
 import { Navbar } from "../components/Navbar";
+import Stepper from "../components/Stepper";
+import { useStepper } from "../hooks/useStepper";
 import { useWidgetMode } from "../hooks/useWidgetMode";
 import { useFetchMaintenanceStatus } from "../stores/maintenanceStore";
 
@@ -13,6 +15,7 @@ interface BaseLayoutProps {
 export const BaseLayout: FC<BaseLayoutProps> = ({ main, modals }) => {
   const isWidgetMode = useWidgetMode();
   const fetchMaintenanceStatus = useFetchMaintenanceStatus();
+  const { steps, currentStep } = useStepper();
 
   // Fetch maintenance status when the app loads
   useEffect(() => {
@@ -34,6 +37,9 @@ export const BaseLayout: FC<BaseLayoutProps> = ({ main, modals }) => {
       {modals}
       <Navbar />
       <MaintenanceBanner />
+      <div className="container mx-auto px-4 pt-6 md:w-120">
+        <Stepper className="mb-6" currentStep={currentStep} steps={steps} />
+      </div>
       {main}
       {!isWidgetMode && <Footer />}
     </>
