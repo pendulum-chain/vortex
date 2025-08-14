a3af6d16ac7103bf8cc38d5ee1f36845a7392fda
Marcel Ebert <mail@marcel-ebert.de>
2025-08-14 20:17:01 +0200

    Use different icon import

--- apps/frontend/src/components/QuoteSummary/index.tsx
+++ apps/frontend/src/components/QuoteSummary/index.tsx
@@ -1,43 +1,25 @@
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/20/solid";
-import { FiatTokenDetails, RampCurrency } from "@packages/shared";
-import { QuoteResponse } from "@packages/shared/src/endpoints/quote.endpoints";
-import { TOKEN_CONFIG } from "@packages/shared/src/tokens/tokenConfig";
+import { QuoteResponse } from "@packages/shared";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import Arrow from "../../assets/arrow.svg";
-import { FiatIcon } from "../FiatIcon";
+import { useGetAssetIcon } from "../../hooks/useGetAssetIcon";
 import { CopyablePublicKey } from "../PublicKey/CopyablePublicKey";
 
 interface QuoteSummaryProps {
   quote: QuoteResponse;
 }
 
-const getFiatTokenDetails = (currency: RampCurrency): FiatTokenDetails | undefined => {
-  const fiatToken = TOKEN_CONFIG[currency as keyof typeof TOKEN_CONFIG];
-  if (fiatToken) {
-    // This is a hack, we need a proper way to get the fiat details
-    return {
-      assetSymbol: currency,
-      fiat: {
-        assetIcon: "",
-        name: currency.toUpperCase(),
-        symbol: currency.toUpperCase()
-      }
-    } as FiatTokenDetails;
-  }
-  return undefined;
-};
-
 export const QuoteSummary = ({ quote }: QuoteSummaryProps) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const toggleExpanded = () => setIsExpanded(!isExpanded);
 
-  const inputFiatDetails = getFiatTokenDetails(quote.inputCurrency);
-  const outputFiatDetails = getFiatTokenDetails(quote.outputCurrency);
+  const inputIcon = useGetAssetIcon(quote.inputCurrency.toLowerCase());
+  const outputIcon = useGetAssetIcon(quote.outputCurrency.toLowerCase());
 
   return (
     <motion.div
-      className="rounded-lg shadow-md p-4 bg-white border border-blue-700"
+      className="rounded-lg border border-blue-700 bg-white p-4 shadow-md"
       layout
       transition={{ duration: 0.3, type: "spring" }}
     >
@@ -45,7 +27,7 @@ export const QuoteSummary = ({ quote }: QuoteSummaryProps) => {
         {!isExpanded ? (
           <motion.div
             animate={{ opacity: 1 }}
-            className="flex justify-between items-center"
+            className="flex items-center justify-between"
             exit={{ opacity: 0 }}
             initial={{ opacity: 0 }}
             key="collapsed"
@@ -56,42 +38,42 @@ export const QuoteSummary = ({ quote }: QuoteSummaryProps) => {
                 <CopyablePublicKey inline={true} publicKey={quote.id} variant="shorter" wrap={true} />
               </div>
             </div>
-            <div className="flex-grow border-l border-gray-300 mx-4 h-12" />
-            <div className="text-sm text-left">
+            <div className="mx-4 h-12 flex-grow border-gray-300 border-l" />
+            <div className="text-left text-sm">
               <div className="flex items-center">
                 <div className="text-gray-500">
                   {quote.inputAmount} {quote.inputCurrency.toUpperCase()}
                 </div>
-                <img alt="arrow" className="w-3 h-3 transform rotate-90 mx-2" src={Arrow} />
+                <img alt="arrow" className="mx-2 h-3 w-3 rotate-90 transform" src={Arrow} />
               </div>
               <div className="font-bold">
                 {quote.outputAmount} {quote.outputCurrency.toUpperCase()}
               </div>
             </div>
-            <button className="p-2 ml-4 btn btn-sm h-8! bg-blue-100 rounded-full" onClick={toggleExpanded} type="button">
+            <button className="btn btn-sm ml-4 h-8! rounded-full bg-blue-100 p-2" onClick={toggleExpanded} type="button">
               <ChevronDownIcon className="h-4 w-4" />
             </button>
           </motion.div>
         ) : (
           <motion.div animate={{ opacity: 1 }} exit={{ opacity: 0 }} initial={{ opacity: 0 }} key="expanded">
-            <div className="flex justify-between items-center">
-              <h2 className="text-lg font-bold">Exchange details</h2>
-              <button className="p-2 btn btn-sm h-8! bg-blue-100 rounded-full" onClick={toggleExpanded} type="button">
+            <div className="flex items-center justify-between">
+              <h2 className="font-bold text-lg">Exchange details</h2>
+              <button className="btn btn-sm h-8! rounded-full bg-blue-100 p-2" onClick={toggleExpanded} type="button">
                 <ChevronUpIcon className="h-4 w-4" />
               </button>
             </div>
-            <div className="grid grid-cols-2 gap-4 mt-4">
+            <div className="mt-4 grid grid-cols-2 gap-4">
               <div className="flex flex-col">
                 <div className="text-gray-500">You send</div>
-                <div className="flex items-center font-bold grow">
-                  {inputFiatDetails && <FiatIcon className="w-6 h-6 mr-2" fiat={inputFiatDetails} />}
+                <div className="flex grow items-center font-bold">
+                  <img alt={quote.inputCurrency} className="mr-2 h-6 w-6" src={inputIcon} />
                   {quote.inputAmount} {quote.inputCurrency.toUpperCase()}
                 </div>
               </div>
               <div className="flex flex-col">
                 <div className="text-gray-500">You get</div>
-                <div className="flex items-center font-bold justify-end grow">
-                  {outputFiatDetails && <FiatIcon className="w-6 h-6 mr-2" fiat={outputFiatDetails} />}~ {quote.outputAmount}{" "}
+                <div className="flex grow items-center justify-end font-bold">
+                  <img alt={quote.outputCurrency} className="mr-2 h-6 w-6" src={outputIcon} />~ {quote.outputAmount}{" "}
                   {quote.outputCurrency.toUpperCase()}
                 </div>
               </div>
