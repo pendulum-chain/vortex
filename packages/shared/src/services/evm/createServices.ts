f6d8e3e0fbbb054d48b59c0fb76edf083d89413b
Gianfranco <g.tasteri@gmail.com>
2025-08-18 14:05:07 -0300

    fix evm transport selection

--- packages/shared/src/services/evm/createServices.ts
+++ packages/shared/src/services/evm/createServices.ts
@@ -2,13 +2,14 @@ import { MOONBEAM_WSS } from "@packages/shared";
 import { createConfig } from "@wagmi/core";
 import { Chain, createPublicClient, createWalletClient, http, webSocket } from "viem";
 import { mnemonicToAccount, privateKeyToAccount } from "viem/accounts";
+import { moonbeam } from "viem/chains";
 
 // @ts-ignore
 export function createEvmClientsAndConfig(
   executorAccount: ReturnType<typeof mnemonicToAccount> | ReturnType<typeof privateKeyToAccount>,
   chain: Chain
 ) {
-  const transport = chain.name === "moonbeam" ? webSocket(MOONBEAM_WSS) : http();
+  const transport = chain.name === moonbeam.name ? webSocket(MOONBEAM_WSS) : http();
   const walletClient = createWalletClient({
     account: executorAccount,
     chain,
