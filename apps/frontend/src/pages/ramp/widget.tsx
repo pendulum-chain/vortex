5afb8c533bde27731b11607706e4dea3c62f5017
Marcel Ebert <mail@marcel-ebert.de>
2025-08-21 18:14:45 +0200

    Use ramp summary button in widget

--- apps/frontend/src/pages/ramp/widget.tsx
+++ apps/frontend/src/pages/ramp/widget.tsx
@@ -6,27 +6,17 @@ import { BrlaSwapFields } from "../../components/BrlaComponents/BrlaSwapFields";
 import { ConnectWalletButton } from "../../components/buttons/ConnectWalletButton";
 import { QuoteSummary } from "../../components/QuoteSummary";
 import { RampSummaryDialog } from "../../components/RampSummaryDialog";
+import { RampSummaryButton } from "../../components/RampSummaryDialog/RampSummaryButton";
 import { useRampActor } from "../../contexts/rampState";
 import { useRampForm } from "../../hooks/ramp/useRampForm";
 import { useRampSubmission } from "../../hooks/ramp/useRampSubmission";
 import { useSetRampUrlParams } from "../../hooks/useRampUrlParams";
 import { useQuote } from "../../stores/quote/useQuoteStore";
 
-function NextButton({ onClick, type = "submit" }: { onClick?: () => void; type?: "button" | "submit" }) {
-  const { t } = useTranslation();
-
-  return (
-    <button className="btn-vortex-primary btn mt-auto mb-4 rounded-xl" onClick={onClick} type={type}>
-      {t("pages.widget.details.buttons.next")}
-    </button>
-  );
-}
-
 function BrazilLanding() {
   return (
     <div className="mx-auto flex h-full w-full flex-col justify-center">
       <BrlaSwapFields />
-      <NextButton />
     </div>
   );
 }
@@ -35,7 +25,6 @@ function EuroLanding() {
   return (
     <div className="mx-auto flex h-full w-full flex-col justify-center">
       <ConnectWalletButton customStyles="w-full btn-vortex-primary btn rounded-xl" hideIcon={true} />
-      <NextButton />
     </div>
   );
 }
@@ -56,30 +45,25 @@ export const WidgetDetailsPage = () => {
 
   const { onRampConfirm } = useRampSubmission();
 
-  const content = rampSummaryVisible ? (
+  return (
     <motion.div
       animate={{ opacity: 1, scale: 1 }}
-      className="mx-4 mt-8 mb-4 flex min-h-[480px] flex-col rounded-lg px-4 pt-4 pb-2 shadow-custom md:mx-auto md:w-96"
+      className="mx-6 mt-8 mb-4 flex min-h-[480px] flex-col rounded-lg px-6 pt-4 pb-2 shadow-custom md:mx-auto md:w-96"
       initial={{ opacity: 0, scale: 0.9 }}
       transition={{ duration: 0.3 }}
     >
-      <RampSummaryDialog />
+      {rampSummaryVisible ? (
+        <RampSummaryDialog />
+      ) : (
+        <FormProvider {...form}>
+          <form className="flex grow flex-col" onSubmit={form.handleSubmit(onRampConfirm)}>
+            <h1 className="mt-2 mb-4 text-center font-bold text-3xl text-blue-700">{t("pages.widget.details.title")}</h1>
+            <div className="mt-8 grid flex-grow gap-3 px-2">{isBrazilLanding ? <BrazilLanding /> : <EuroLanding />}</div>
+            <RampSummaryButton className="mb-4" />
+          </form>
+        </FormProvider>
+      )}
+      <div className="mt-auto mb-2">{quote && <QuoteSummary quote={quote} />}</div>
     </motion.div>
-  ) : (
-    <FormProvider {...form}>
-      <motion.form
-        animate={{ opacity: 1, scale: 1 }}
-        className="mx-4 mt-8 mb-4 flex min-h-[480px] flex-col rounded-lg px-4 pt-4 pb-2 shadow-custom md:mx-auto md:w-96"
-        initial={{ opacity: 0, scale: 0.9 }}
-        onSubmit={form.handleSubmit(onRampConfirm)}
-        transition={{ duration: 0.3 }}
-      >
-        <h1 className="mt-2 mb-4 text-center font-bold text-3xl text-blue-700">{t("pages.widget.details.title")}</h1>
-        <div className="mt-8 grid flex-grow gap-3 px-2">{isBrazilLanding ? <BrazilLanding /> : <EuroLanding />}</div>
-        <div className="mt-auto mb-8 grid gap-3 px-2">{quote && <QuoteSummary quote={quote} />}</div>
-      </motion.form>
-    </FormProvider>
   );
-
-  return content;
 };
