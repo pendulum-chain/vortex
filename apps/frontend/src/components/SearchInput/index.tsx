0a352f47c7a78145381674df9d3b24297524dd66
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-08-29 09:14:00 +0100

    use cn for SearchInput

--- apps/frontend/src/components/SearchInput/index.tsx
+++ apps/frontend/src/components/SearchInput/index.tsx
@@ -1,6 +1,7 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { Dispatch, SetStateAction } from "react";
 import { useTranslation } from "react-i18next";
+import { cn } from "../../helpers/cn";
 
 interface SearchInputProps {
   set: Dispatch<SetStateAction<string>> | ((value: string) => void);
@@ -13,13 +14,16 @@ export const SearchInput = ({ set, placeholder, className, ...p }: SearchInputPr
 
   return (
     <label
-      className={` ${className || "w-full"} input-vortex-primary input input-bordered flex h-[3rem] items-center text-base focus:outline-primary`}
+      className={cn(
+        "input-vortex-primary input input-bordered flex h-[3rem] items-center text-base focus:outline-primary",
+        className ?? "w-full"
+      )}
       htmlFor="search"
     >
       <MagnifyingGlassIcon className="mr-1 size-5 text-neutral-400" />
       <input
         autoComplete="off"
-        className="h-[3rem] w-full "
+        className="h-[3rem] w-full"
         id="search"
         name="search"
         onChange={e => set((e.target as HTMLInputElement).value)}
