22128ef7d94e41b935234c665f2fd00fc1f1cd30
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-09-10 22:25:24 +0200

    create SettingsButton

---
+++ apps/frontend/src/components/SettingsMenu/SettingsButton/index.tsx
@@ -0,0 +1,21 @@
+import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+import { useSettingsMenuActions, useSettingsMenuState } from "../../../stores/settingsMenuStore";
+import { SettingsMenu } from "..";
+
+export const SettingsButton = () => {
+  const isOpen = useSettingsMenuState();
+  const { toggleMenu } = useSettingsMenuActions();
+
+  return (
+    <>
+      <button
+        aria-label={isOpen ? "Close menu" : "Open menu"}
+        className={"btn-vortex-accent cursor-pointer rounded-full p-2 px-3.5 py-1.5"}
+        onClick={toggleMenu}
+      >
+        <Cog6ToothIcon className="h-5 w-5" />
+      </button>
+      <SettingsMenu />
+    </>
+  );
+};
