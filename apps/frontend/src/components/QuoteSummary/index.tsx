e81f9a85cb7df2f2395c84e9b839fed85a99f861
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-09-17 15:19:51 +0200

    display icons in QuoteSummary

--- apps/frontend/src/components/QuoteSummary/index.tsx
+++ apps/frontend/src/components/QuoteSummary/index.tsx
@@ -1,4 +1,5 @@
 import { QuoteResponse } from "@packages/shared";
+import { useGetAssetIcon } from "../../hooks/useGetAssetIcon";
 import { CollapsibleCard, CollapsibleDetails, CollapsibleSummary, useCollapsibleCard } from "../CollapsibleCard";
 import { CurrencyExchange } from "../CurrencyExchange";
 import { ToggleButton } from "../ToggleButton";
@@ -35,21 +36,26 @@ const QuoteSummaryCore = ({ quote }: { quote: QuoteResponse }) => {
 };
 
 const QuoteSummaryDetails = ({ quote }: { quote: QuoteResponse }) => {
+  const inputIcon = useGetAssetIcon(quote.inputCurrency.toLowerCase());
+  const outputIcon = useGetAssetIcon(quote.outputCurrency.toLowerCase());
+
   return (
-    <>
+    <section className="overflow-hidden">
       <div className="mb-4">
         <h3 className="mb-3 font-semibold text-gray-900">Exchange Details</h3>
         <div className="grid grid-cols-2 gap-4">
           <div className="flex flex-col">
             <div className="text-gray-500 text-sm">You Send</div>
             <div className="flex items-center font-bold">
+              <img alt={quote.inputCurrency} className="mr-2 h-5 w-5" src={inputIcon} />
               {quote.inputAmount} {quote.inputCurrency.toUpperCase()}
             </div>
           </div>
           <div className="flex flex-col">
             <div className="text-gray-500 text-sm">You Receive</div>
             <div className="flex items-center font-bold">
-              ~ {quote.outputAmount} {quote.outputCurrency.toUpperCase()}
+              <img alt={quote.outputCurrency} className="mr-2 h-5 w-5" src={outputIcon} />~ {quote.outputAmount}{" "}
+              {quote.outputCurrency.toUpperCase()}
             </div>
           </div>
         </div>
@@ -57,19 +63,21 @@ const QuoteSummaryDetails = ({ quote }: { quote: QuoteResponse }) => {
       <div>
         <TransactionId id={quote.id} label="Full Transaction ID" variant="full" />
       </div>
-    </>
+    </section>
   );
 };
 
 export const QuoteSummary = ({ quote }: QuoteSummaryProps) => {
   return (
-    <CollapsibleCard>
-      <CollapsibleSummary>
-        <QuoteSummaryCore quote={quote} />
-      </CollapsibleSummary>
-      <CollapsibleDetails>
-        <QuoteSummaryDetails quote={quote} />
-      </CollapsibleDetails>
-    </CollapsibleCard>
+    <section className="max-w-[336px]">
+      <CollapsibleCard>
+        <CollapsibleSummary>
+          <QuoteSummaryCore quote={quote} />
+        </CollapsibleSummary>
+        <CollapsibleDetails>
+          <QuoteSummaryDetails quote={quote} />
+        </CollapsibleDetails>
+      </CollapsibleCard>
+    </section>
   );
 };
