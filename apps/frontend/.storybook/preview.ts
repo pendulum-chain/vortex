7f6d461f64ddb2f294f03a758cf9da72c3059164
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-09-17 14:31:04 +0200

    configure i18n for Storybook

--- apps/frontend/.storybook/preview.ts
+++ apps/frontend/.storybook/preview.ts
@@ -1,5 +1,23 @@
 import type { Preview } from "@storybook/react-vite";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslations from "../src/translations/en.json";
+import { Language } from "../src/translations/helpers";
+import ptTranslations from "../src/translations/pt.json";
 import "../App.css";
+
+i18n.use(initReactI18next).init({
+  fallbackLng: "en",
+  lng: "en",
+  resources: {
+    [Language.English]: {
+      translation: enTranslations
+    },
+    [Language.Portuguese_Brazil]: {
+      translation: ptTranslations
+    }
+  }
+});
 
 const preview: Preview = {
   parameters: {
