1e7b40aa4935ec1693253cd386e7472c02e42a4a
Gianfranco <g.tasteri@gmail.com>
2025-09-24 10:33:43 -0300

    WIP

--- apps/frontend/src/machines/actors/brla/createSubaccount.actor.ts
+++ apps/frontend/src/machines/actors/brla/createSubaccount.actor.ts
@@ -1,29 +1,54 @@
-import { AveniaAccountType } from "@packages/shared";
+import { AveniaAccountType, BrlaGetKycStatusResponse, KycAttemptStatus } from "@packages/shared";
 import { fromPromise } from "xstate";
+import { isValidCnpj } from "../../../hooks/ramp/schema";
 import { BrlaService } from "../../../services/api";
-import { createSubaccount, KycSubmissionNetworkError } from "../../../services/signingService";
+import { createSubaccount, fetchKycStatus } from "../../../services/signingService";
 import { AveniaKycContext } from "../../kyc.states";
 
 export const createSubaccountActor = fromPromise(
-  async ({ input }: { input: AveniaKycContext }): Promise<{ subAccountId: string }> => {
+  async ({
+    input
+  }: {
+    input: AveniaKycContext;
+  }): Promise<{ subAccountId: string; maybeKycAttemptStatus?: BrlaGetKycStatusResponse }> => {
     const { taxId, kycFormData } = input;
 
     let subAccountId: string;
+    let maybeKycAttemptStatus: BrlaGetKycStatusResponse | undefined;
     if (!kycFormData) {
       throw new Error("Invalid input state. This is a Bug.");
     }
 
     try {
       ({ subAccountId } = await BrlaService.getUser(taxId));
+
+      try {
+        maybeKycAttemptStatus = await fetchKycStatus(taxId);
+      } catch (e) {
+        console.log("Debug: could not fetch kyc status", e);
+        // It's fine if this fails, we just won't have the status.
+      }
+
+      if (
+        maybeKycAttemptStatus?.status === KycAttemptStatus.PENDING ||
+        maybeKycAttemptStatus?.status === KycAttemptStatus.PROCESSING
+      ) {
+        return { maybeKycAttemptStatus, subAccountId };
+      }
+
       return { subAccountId };
     } catch (error: unknown) {
       console.log("Debug: failed to fetch existing Avenia subaccount", error);
-      const err = error as string;
-      console.log("type fo err", typeof String(err));
+
+      if (isValidCnpj(taxId) && !kycFormData.companyName) {
+        throw new Error("createSubaccountActor: Invalid input state. This is a Bug.");
+      }
+
+      const newSubaccountName = isValidCnpj(taxId) ? kycFormData.companyName! : kycFormData.fullName;
 
       ({ subAccountId } = await createSubaccount({
         accountType: AveniaAccountType.INDIVIDUAL,
-        name: kycFormData.fullName,
+        name: newSubaccountName,
         taxId
       }));
 
