a0940c5a567df9448209b9873df90e20570666b3
Marcel Ebert <mail@marcel-ebert.de>
2025-10-02 20:41:33 +0200

    Refactor HydrationRouter

--- apps/api/src/api/services/hydration/swap.ts
+++ apps/api/src/api/services/hydration/swap.ts
@@ -1,27 +1,39 @@
 import { EvmClient, PoolService, Trade, TradeRouter, TxBuilderFactory } from "@galacticcouncil/sdk";
 import { ApiManager } from "@packages/shared";
 
-export async function getBestSellPriceFor(assetIn: string, assetOut: string, amountIn: string) {
-  const apiManager = ApiManager.getInstance();
-  const { api } = await apiManager.getApi("hydration");
-  const evmClient = new EvmClient(api);
+export class HydrationRouter {
+  private tradeRouter: TradeRouter;
+  private poolService: PoolService;
+  private evmClient: EvmClient;
+  private txBuilderFactory: TxBuilderFactory;
 
-  const poolService = new PoolService(api, evmClient);
-  const tradeRouter = new TradeRouter(poolService);
-  await poolService.syncRegistry();
+  constructor() {
+    const apiManager = ApiManager.getInstance();
+    apiManager.getApi("hydration").then(({ api }) => {
+      const evmClient = new EvmClient(api);
+      this.evmClient = evmClient;
 
-  // Calculate spot price
-  return await tradeRouter.getBestSell(assetIn, assetOut, amountIn);
+      this.poolService = new PoolService(api, evmClient);
+      this.tradeRouter = new TradeRouter(this.poolService);
+      this.poolService.syncRegistry();
+
+      this.txBuilderFactory = new TxBuilderFactory(api, evmClient);
+
+      console.log("Constructed HydrationRouter");
+    });
+  }
+  async getBestSellPriceFor(assetIn: string, assetOut: string, amountIn: string) {
+    // Calculate spot price
+    return await this.tradeRouter.getBestSell(assetIn, assetOut, amountIn);
+  }
+
+  async createTransactionForTrade(trade: Trade, beneficiaryAddress: string, slippage = 0.5) {
+    const txBuilder = this.txBuilderFactory.trade(trade);
+    txBuilder.withBeneficiary(beneficiaryAddress);
+    txBuilder.withSlippage(slippage);
+
+    return await txBuilder.build();
+  }
 }
 
-export async function createTransactionForTrade(trade: Trade, beneficiaryAddress: string, slippage = 0.5) {
-  const apiManager = ApiManager.getInstance();
-  const { api } = await apiManager.getApi("hydration");
-  const evmClient = new EvmClient(api);
-
-  const txBuilder = new TxBuilderFactory(api, evmClient).trade(trade);
-  txBuilder.withBeneficiary(beneficiaryAddress);
-  txBuilder.withSlippage(slippage);
-
-  return await txBuilder.build();
-}
+export default new HydrationRouter();
