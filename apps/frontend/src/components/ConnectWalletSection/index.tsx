f4c86cd81945b9a788c55cce9fa19bb3cf7fe031
Marcel Ebert <mail@marcel-ebert.de>
2025-10-13 21:22:33 +0200

    Pass forceNetwork prop

--- apps/frontend/src/components/ConnectWalletSection/index.tsx
+++ apps/frontend/src/components/ConnectWalletSection/index.tsx
@@ -1,20 +1,21 @@
+import { Networks } from "@packages/shared";
 import { cn } from "../../helpers/cn";
 import { useVortexAccount } from "../../hooks/useVortexAccount";
 import { ConnectWalletButton, WalletButtonVariant } from "../buttons/ConnectWalletButton";
 
 export interface ConnectWalletSectionProps {
   className?: string;
+  forceNetwork?: Networks;
 }
 
-export const ConnectWalletSection = ({ className }: ConnectWalletSectionProps) => {
+export const ConnectWalletSection = ({ className, forceNetwork }: ConnectWalletSectionProps) => {
   const { isConnected } = useVortexAccount();
-
   return (
     <div className={cn("mb-4 w-full", isConnected && "mb-2", className)}>
       {isConnected ? (
-        <ConnectWalletButton customStyles="w-full" variant={WalletButtonVariant.Minimal} />
+        <ConnectWalletButton customStyles="w-full" forceNetwork={forceNetwork} variant={WalletButtonVariant.Minimal} />
       ) : (
-        <ConnectWalletButton customStyles="w-full" hideIcon />
+        <ConnectWalletButton customStyles="w-full" forceNetwork={forceNetwork} hideIcon />
       )}
     </div>
   );
