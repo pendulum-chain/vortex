9b63a7d99f9dbc3c204f2b403e5b2e92170e80e4
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-10-14 15:04:29 +0200

    add a migration for the new quote_tickets fields

---
+++ apps/api/src/database/migrations/015-add-quote-fields.ts
@@ -0,0 +1,24 @@
+import { DataTypes, QueryInterface } from "sequelize";
+
+export async function up(queryInterface: QueryInterface): Promise<void> {
+  await queryInterface.addColumn("quote_tickets", "payment_method", {
+    allowNull: true,
+    type: DataTypes.STRING(20)
+  });
+
+  await queryInterface.addColumn("quote_tickets", "country_code", {
+    allowNull: true,
+    type: DataTypes.STRING(2)
+  });
+
+  await queryInterface.addColumn("quote_tickets", "network", {
+    allowNull: true,
+    type: DataTypes.STRING(20)
+  });
+}
+
+export async function down(queryInterface: QueryInterface): Promise<void> {
+  await queryInterface.removeColumn("quote_tickets", "payment_method");
+  await queryInterface.removeColumn("quote_tickets", "country_code");
+  await queryInterface.removeColumn("quote_tickets", "network");
+}
