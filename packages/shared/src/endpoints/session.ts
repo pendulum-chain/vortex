262be30eec492a5d9c893babfdc800088c69bbc2
Marcel Ebert <mail@marcel-ebert.de>
2025-10-17 10:28:58 +0200

    Refactor widget URL interfaces to include additional parameters and improve type definitions

--- packages/shared/src/endpoints/session.ts
+++ packages/shared/src/endpoints/session.ts
@@ -1,18 +1,29 @@
-import { CreateQuoteRequest } from "./quote.endpoints";
+import { FiatToken, Networks, OnChainToken, PaymentMethod, RampDirection } from "@packages/shared";
 
 export interface GetWidgetUrlLocked {
   quoteId: string;
+  callbackUrl?: string;
   externalSessionId: string;
-  walletAddressLocked?: string;
   externalTransactionId?: string;
   externalCustomerId?: string;
+  walletAddressLocked?: string;
 }
 
-export interface GetWidgetUrlRefresh extends CreateQuoteRequest {
-  externalSessionId: string;
-  walletAddressLocked?: string;
-  externalTransactionId?: string;
+export interface GetWidgetUrlRefresh {
+  callbackUrl?: string;
+  countryCode?: string;
+  crypto?: OnChainToken;
+  cryptoLocked?: OnChainToken;
   externalCustomerId?: string;
+  externalSessionId: string;
+  externalTransactionId?: string;
+  fiat?: FiatToken;
+  inputAmount: string;
+  network: Networks;
+  partnerId?: string;
+  paymentMethod?: PaymentMethod;
+  rampType: RampDirection;
+  walletAddressLocked?: string;
 }
 
 export interface GetWidgetUrlResponse {
