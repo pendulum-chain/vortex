52078c223a18b8fc04113a17349399c5f8e93258
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-10-30 17:09:43 +0100

    remove unnecessary useEffect in useRampForm

--- apps/frontend/src/hooks/ramp/useRampForm.ts
+++ apps/frontend/src/hooks/ramp/useRampForm.ts
@@ -1,5 +1,4 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useEffect, useRef } from "react";
 import { UseFormReturn, useForm } from "react-hook-form";
 import { RampFormValues, useSchema } from "./schema";
 
@@ -15,17 +14,6 @@ export const useRampForm = (
     defaultValues,
     resolver: yupResolver(formSchema)
   });
-
-  const isAddressSet = useRef(false);
-
-  // Only sets the wallet address once. Then it respects the user's choice of
-  // onramp destination.
-  useEffect(() => {
-    if (defaultValues?.walletAddress && !isAddressSet.current) {
-      form.setValue("walletAddress", defaultValues.walletAddress);
-      isAddressSet.current = true;
-    }
-  }, [defaultValues?.walletAddress, form.setValue]);
 
   return {
     form,
