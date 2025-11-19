2ae321d5221fdf2337e2ab88fb5c65c2fa7091ba
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-19 19:26:41 +0100

    implement animate-slide-up custom class

--- apps/frontend/src/pages/main/index.tsx
+++ apps/frontend/src/pages/main/index.tsx
@@ -16,7 +16,7 @@ export const Main = () => {
         <>
           <div className="relative overflow-hidden bg-[radial-gradient(at_74%_98%,theme(colors.blue.900),theme(colors.blue.950),theme(colors.blue.950))] pb-4">
             <div className="container mx-auto flex grow-1 flex-col items-center justify-between lg:flex-row">
-              <div className="flex flex-col gap-4 mr-5">
+              <div className="flex flex-col gap-4 mr-5 animate-slide-up">
                 <div className="pt-8 text-center font-semibold text-3xl md:text-5xl text-white lg:text-6xl lg:pt-0 lg:text-start">
                   <Trans i18nKey="pages.main.hero.title">
                     <span className="text-blue-400">How to sell</span> cryptocurrency online <br /> with Vortex Finance
