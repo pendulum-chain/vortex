68837d8d0db682ca895b6f1ac78ca8b3fae092b9
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-19 11:51:46 +0100

    create MobileNavbar component

---
+++ apps/frontend/src/components/Navbar/MobileNavbar.tsx
@@ -0,0 +1,53 @@
+import { AnimatePresence } from "motion/react";
+import { useRef, useState } from "react";
+import { useClickOutside } from "../../hooks/useClickOutside";
+import { useWidgetMode } from "../../hooks/useWidgetMode";
+import { HamburgerButton } from "./HamburgerButton";
+import { useNavbarHandlers } from "./hooks/useNavbarHandlers";
+import { LogoButton } from "./LogoButton";
+import { MobileMenu } from "./MobileMenu";
+
+export const MobileNavbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const isWidgetMode = useWidgetMode();
+  const navbarRef = useRef<HTMLDivElement>(null);
+
+  const { resetRampAndNavigateHome } = useNavbarHandlers();
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
+  useClickOutside(navbarRef, closeMobileMenu, isMobileMenuOpen);
+
+  return (
+    <div ref={navbarRef}>
+      <div className="relative z-50 bg-blue-950 px-4 py-4 md:px-10 md:py-5">
+        <div className="flex items-center justify-between">
+          {isWidgetMode ? (
+            <LogoButton onClick={resetRampAndNavigateHome} />
+          ) : (
+            <div className="flex grow items-center justify-between">
+              <LogoButton onClick={resetRampAndNavigateHome} />
+              <HamburgerButton isOpen={isMobileMenuOpen} onClick={toggleMobileMenu} />
+            </div>
+          )}
+        </div>
+      </div>
+      <AnimatePresence>
+        {isMobileMenuOpen && (
+          <MobileMenu
+            onBookDemoClick={() => {
+              console.log("book demo");
+            }}
+            onMenuItemClick={closeMobileMenu}
+          />
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
