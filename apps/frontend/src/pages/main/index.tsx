d8f3603220c34e9ae098b4737934e54472cdff2f
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-19 11:51:08 +0100

    change text of the landing section (individuals)

--- apps/frontend/src/pages/main/index.tsx
+++ apps/frontend/src/pages/main/index.tsx
@@ -15,15 +15,16 @@ export const Main = () => {
       {!isWidgetMode ? (
         <>
           <div className="relative overflow-hidden bg-[radial-gradient(at_74%_98%,theme(colors.blue.900),theme(colors.blue.950),theme(colors.blue.950))] pb-4">
-            <div className="flex grow-1 flex-col items-center justify-evenly md:flex-row">
-              <div className="pt-8 text-center font-semibold text-2xl text-white md:pt-0 md:text-3xl lg:text-end lg:text-4xl">
-                <Trans
-                  components={{
-                    1: <span className="text-[rgb(238,201,115)]" />,
-                    br: <br />
-                  }}
-                  i18nKey="pages.main.hero.title"
-                />
+            <div className="container mx-auto flex grow-1 flex-col items-center justify-between lg:flex-row">
+              <div className="flex flex-col gap-4">
+                <div className="pt-8 text-center font-semibold text-3xl text-white sm:text-4xl lg:pt-0 lg:text-start">
+                  <Trans i18nKey="pages.main.hero.title">
+                    <span className="text-blue-400">How to sell</span> cryptocurrency online <br /> with Vortex Finance
+                  </Trans>
+                </div>
+                <p className="text-center text-white sm:text-lg">
+                  <Trans i18nKey="pages.main.hero.subtitle" />
+                </p>
               </div>
               <Quote />
             </div>
