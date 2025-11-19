5d46f5578e3cf42dd0c9da8cd6fdec76f97225d4
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-19 21:46:53 +0100

    update business page

--- apps/frontend/src/pages/business/index.tsx
+++ apps/frontend/src/pages/business/index.tsx
@@ -1,24 +1,29 @@
-import BackgroundImage from "../../assets/bg-1.svg";
 import VortexMetamaskImage from "../../assets/vortex-metamask.png";
-import { BaseLayout } from "../../layouts";
 
 export function BusinessMain() {
-  const main = (
+  return (
     <main>
-      <section className="container mx-auto mt-18 flex gap-10">
-        <div className="flex w-full flex-col justify-center md:w-2/5">
+      <section className="container mx-auto pt-32 flex flex-col lg:flex-row gap-10 relative lg:mb-32">
+        <div className="flex w-full flex-col justify-center lg:w-1/2">
           <div>
-            <p className="pt-8 text-center font-light text-3xl text-black sm:text-4xl lg:pt-0 lg:text-start">
+            <p className="pt-8 text-center font-light text-3xl text-black sm:text-5xl md:text-6xl lg:pt-0 lg:text-start">
               Enable your customers to buy and sell crypto instantly - directly{" "}
               <span className="text-blue-700">inside your APP</span>
             </p>
-            <p className="my-4">
+            <p className="my-6 text-center lg:text-left sm:text-xl">
               With one simple, free integration Vortex handles KYC, compliance and settlement so you can stay focused on your
               product.
             </p>
           </div>
-          <div className="mt-4 flex gap-2">
-            <button className="btn btn-vortex-primary">SDK integration</button>
+          <div className="mt-4 flex gap-2 justify-center lg:justify-start">
+            <a
+              className="btn btn-vortex-primary"
+              href="https://api-docs.vortexfinance.co/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              SDK integration
+            </a>
             <div className="relative">
               <div className="badge absolute top-[-10px] right-[-20px] z-20 bg-blue-700 text-white">Coming Soon</div>
               <button className="btn btn-vortex-primary-inverse" disabled>
@@ -27,18 +32,19 @@ export function BusinessMain() {
             </div>
           </div>
         </div>
-        <div className="flex w-full flex-col gap-4 md:w-3/5">
-          <div className="relative w-full">
-            <img alt="Background" className="absolute right-[-100px] bottom-[-100px] z-1 h-full w-full" src={BackgroundImage} />
-            <img alt="Vortex Metamask" className="relative z-20 w-full" src={VortexMetamaskImage} />
-            {/* <div className="absolute top-[80%] left-[40%] h-full w-full rotate-180 bg-[radial-gradient(circle_at_74%_98%,theme(colors.blue.900),transparent_30%)]">
-              {" "}
-            </div> */}
-          </div>
+        <img
+          alt="Vortex Metamask"
+          className="hover:scale-[1.01] hidden lg:block transition-all duration-300 absolute right-[-140px] bottom-[-80px] w-3/5 z-20"
+          src={VortexMetamaskImage}
+        />
+        <div className="lg:w-1/2 relative">
+          <img
+            alt="Vortex Metamask"
+            className="hover:scale-[1.01] block lg:hidden transition-all duration-300"
+            src={VortexMetamaskImage}
+          />
         </div>
       </section>
     </main>
   );
-
-  return <BaseLayout main={main} />;
 }
