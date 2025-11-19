097cf7780ff2534fe45c6cf359c591f62c053fdd
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-19 21:47:44 +0100

    update business page

--- apps/frontend/src/routes/{-$locale}/business.tsx
+++ apps/frontend/src/routes/{-$locale}/business.tsx
@@ -1,15 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { BaseLayout } from "../../layouts";
 import { BusinessMain } from "../../pages/business";
-import { Main } from "../../pages/main";
+import { WhyVortexBusiness } from "../../sections/WhyVortexBusiness";
 
 export const Route = createFileRoute("/{-$locale}/business")({
   component: BusinessRouteComponent
 });
 
 function BusinessRouteComponent() {
-  return (
+  const main = (
     <>
       <BusinessMain />
+      <WhyVortexBusiness />
     </>
   );
+
+  return <BaseLayout main={main} />;
 }
