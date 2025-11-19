a932e6afebc1880bc4e6b439bfcbf248dca6c7a1
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-19 19:04:03 +0100

    update individuals page h1 text-size

--- apps/frontend/src/pages/main/index.tsx
+++ apps/frontend/src/pages/main/index.tsx
@@ -16,13 +16,13 @@ export const Main = () => {
         <>
           <div className="relative overflow-hidden bg-[radial-gradient(at_74%_98%,theme(colors.blue.900),theme(colors.blue.950),theme(colors.blue.950))] pb-4">
             <div className="container mx-auto flex grow-1 flex-col items-center justify-between lg:flex-row">
-              <div className="flex flex-col gap-4">
-                <div className="pt-8 text-center font-semibold text-3xl text-white sm:text-4xl lg:pt-0 lg:text-start">
+              <div className="flex flex-col gap-4 mr-5">
+                <div className="pt-8 text-center font-semibold text-3xl md:text-5xl text-white lg:text-6xl lg:pt-0 lg:text-start">
                   <Trans i18nKey="pages.main.hero.title">
                     <span className="text-blue-400">How to sell</span> cryptocurrency online <br /> with Vortex Finance
                   </Trans>
                 </div>
-                <p className="text-center text-white sm:text-lg">
+                <p className="text-center sm:text-left text-white sm:text-lg">
                   <Trans i18nKey="pages.main.hero.subtitle" />
                 </p>
               </div>
