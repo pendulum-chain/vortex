156832fab194903852940ddc3f95112b948ab82f
Marcel Ebert <mail@marcel-ebert.de>
2025-11-21 19:01:17 +0100

    Adjust comment

--- apps/api/src/api/services/quote/core/types.ts
+++ apps/api/src/api/services/quote/core/types.ts
@@ -204,7 +204,7 @@ export interface QuoteContext {
     // Ideal subsidy needed to reach expected output (uncapped)
     idealSubsidyAmountInOutputTokenDecimal: Big;
     idealSubsidyAmountInOutputTokenRaw: string;
-    // Target output amount after subsidy
+    // Target output amount after subsidy (actual output + subsidy)
     targetOutputAmountDecimal: Big;
     targetOutputAmountRaw: string;
   };
