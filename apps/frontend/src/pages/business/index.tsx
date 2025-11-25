05e3cc0bc5e3da276dde4651276d21ddce6fa36f
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-25 18:18:57 +0100

    refactor business landing section

--- apps/frontend/src/pages/business/index.tsx
+++ apps/frontend/src/pages/business/index.tsx
@@ -1,22 +1,23 @@
-import BackgroundImage2 from "../../assets/bg-4.svg";
-import VortexMetamaskImage from "../../assets/vortex-metamask.png";
+import WidgetSnippetImageBRL from "../../assets/widget-full-snippet-brl.png";
+import WidgetSnippetImageEUR from "../../assets/widget-full-snippet-eur.png";
+import WidgetSnippetImageSell from "../../assets/widget-full-snippet-sell.png";
 
 export function BusinessMain() {
   return (
     <main>
-      <section className="container mx-auto pt-32 flex flex-col lg:flex-row gap-10 relative lg:mb-32">
-        <div className="flex w-full flex-col justify-center lg:w-1/2">
+      <section className="container mx-auto grid grid-cols-1 gap-x-20 px-4 md:px-10 py-8 gap-y-10 lg:grid-cols-2 grid-rows-2 lg:grid-rows-1 pt-16 md:py-32">
+        <div className="flex flex-col justify-center animate-slide-up">
           <div>
-            <p className="pt-8 text-center font-light text-3xl text-black sm:text-5xl md:text-6xl lg:pt-0 lg:text-start">
+            <h1 className="animate-slide-up text-h1 text-center text-black lg:text-start">
               Enable your customers to buy and sell crypto instantly - directly{" "}
               <span className="text-blue-700">inside your APP</span>
-            </p>
-            <p className="my-6 text-center lg:text-left sm:text-xl">
+            </h1>
+            <p className="animate-slide-up my-6 text-center lg:text-left sm:text-xl">
               With one simple, free integration Vortex handles KYC, compliance and settlement so you can stay focused on your
               product.
             </p>
           </div>
-          <div className="mt-4 flex gap-2 justify-center lg:justify-start">
+          <div className="animate-slide-up mt-4 flex gap-2 justify-center lg:justify-start">
             <a
               className="btn btn-vortex-primary"
               href="https://api-docs.vortexfinance.co/"
@@ -26,28 +27,36 @@ export function BusinessMain() {
               SDK integration
             </a>
             <div className="relative">
-              <div className="badge absolute top-[-10px] right-[-20px] z-20 bg-blue-700 text-white">Coming Soon</div>
-              <button className="btn btn-vortex-primary-inverse" disabled>
+              <div className="animate-slide-up badge absolute top-[-10px] right-[-20px] z-20 bg-blue-700 text-white">
+                Coming Soon
+              </div>
+              <button className="animate-slide-up btn btn-vortex-primary-inverse" disabled>
                 Widget integration
               </button>
             </div>
           </div>
         </div>
-        <img
-          alt="Background"
-          className="hidden lg:block absolute right-[-100px] bottom-[-200px] z-1 w-[300px] h-[300px]"
-          src={BackgroundImage2}
-        />
-        <img
-          alt="Vortex Metamask"
-          className="hover:scale-[1.01] hidden lg:block transition-all duration-300 absolute right-[-140px] bottom-[-80px] w-3/5 z-20"
-          src={VortexMetamaskImage}
-        />
-        <div className="lg:w-1/2 relative">
+
+        <div className="relative flex justify-around items-center w-flex">
           <img
-            alt="Vortex Metamask"
-            className="hover:scale-[1.01] block lg:hidden transition-all duration-300"
-            src={VortexMetamaskImage}
+            alt="Widget Snippet EUR"
+            className="hover:scale-[1.11] animate-slide-up transition-all cursor-pointer duration-300 shadow-custom rounded-lg w-1/3 z-10 hover:z-30"
+            draggable={false}
+            src={WidgetSnippetImageEUR}
+          />
+
+          <img
+            alt="Widget Snippet Sell"
+            className="hover:scale-[1.11] scale-[1.10] animate-slide-up transition-all cursor-pointer duration-300 shadow-custom rounded-lg w-1/3 z-20 hover:z-30"
+            draggable={false}
+            src={WidgetSnippetImageSell}
+          />
+
+          <img
+            alt="Widget Snippet BRL"
+            className="hover:scale-[1.11] animate-slide-up transition-all cursor-pointer duration-300 shadow-custom rounded-lg w-1/3 z-10 hover:z-30"
+            draggable={false}
+            src={WidgetSnippetImageBRL}
           />
         </div>
       </section>
