79fd0b9e50914b93f2cbde7f2189ff770bcfcb74
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-25 18:21:05 +0100

    refactor paddings, texts GotQuestions section

--- apps/frontend/src/sections/GotQuestions/index.tsx
+++ apps/frontend/src/sections/GotQuestions/index.tsx
@@ -1,6 +1,6 @@
 import { PlayCircleIcon } from "@heroicons/react/20/solid";
 import { motion } from "motion/react";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import PLANET from "../../assets/planet.svg";
 
 const FORM_URL = "https://forms.gle/dKh8ckXheRPdRa398";
@@ -9,7 +9,7 @@ export const GotQuestions = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="overflow-hidden bg-blue-900 px-8 py-32 text-white">
+    <section className="overflow-hidden bg-blue-900 px-4 md:px-10 py-32 text-white">
       <div className="relative mx-auto flex flex-col justify-between sm:container md:flex-row">
         <motion.img
           alt="Planet"
@@ -22,10 +22,14 @@ export const GotQuestions = () => {
           whileInView={{ opacity: 1, rotate: 0, x: 0, y: 0 }}
         />
         <div className="z-10 md:w-1/2">
-          <h1 className="text-4xl">{t("sections.gotQuestions.title")}</h1>
+          <h1 className="text-h1 md:text-start text-center font-bold md:font-normal">
+            <Trans i18nKey="sections.gotQuestions.title">
+              Got Questions? <br /> We're here to help!
+            </Trans>
+          </h1>
         </div>
-        <div className="z-10 flex flex-col md:w-1/2 md:items-end">
-          <p className="mt-3 mb-4 text-lg md:mt-0 md:text-end">{t("sections.gotQuestions.description")}</p>
+        <div className="z-10 flex flex-col md:w-1/2 md:items-end justify-center">
+          <p className="text-body-lg mt-3 mb-4 md:mt-0 md:text-end text-center">{t("sections.gotQuestions.description")}</p>
           <a className="btn btn-vortex-secondary mx-auto flex items-center gap-2 rounded-3xl px-6 md:mx-0" href={FORM_URL}>
             <span>{t("sections.gotQuestions.contactUs")}</span>
             <PlayCircleIcon className="w-5 group-hover:text-pink-600" />
