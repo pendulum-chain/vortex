1cb86ea4d8a24462af1dc771d53d6b678cf298d2
Marcel Ebert <mail@marcel-ebert.de>
2026-01-24 17:35:31 +0000

    Add entry for axlUSDC on Ethereum

--- packages/shared/src/tokens/evm/config.ts
+++ packages/shared/src/tokens/evm/config.ts
@@ -42,7 +42,7 @@ export const evmTokenConfig: Record<EvmNetworks, Partial<Record<EvmToken, EvmTok
     [EvmToken.AXLUSDC]: {
       assetSymbol: "axlUSDC",
       decimals: 6,
-      erc20AddressSourceChain: "0x....",
+      erc20AddressSourceChain: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
       isNative: false,
       network: Networks.Ethereum,
       networkAssetIcon: "ethereumUSDC",
