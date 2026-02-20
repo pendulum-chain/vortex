094244be7c1ce537b198105732375e3edf64455d
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2026-02-20 16:26:04 +0100

    fix: replace spacer div with bottom padding in quote page

--- apps/frontend/src/pages/quote/index.tsx
+++ apps/frontend/src/pages/quote/index.tsx
@@ -14,10 +14,9 @@ export const Quote = () => {
 
   return (
     <main>
-      <div className="relative mx-4 mt-8 mb-4 animate-appear overflow-hidden rounded-lg bg-white px-4 pt-4 pb-2 shadow-custom md:mx-auto md:w-96">
+      <div className="relative mx-4 mt-8 mb-4 animate-appear overflow-hidden rounded-lg bg-white px-4 pt-4 pb-18 shadow-custom md:mx-auto md:w-96">
         <RampToggle activeDirection={activeSwapDirection} onToggle={onSwapDirectionToggle} />
         {activeSwapDirection === RampDirection.BUY ? <Onramp /> : <Offramp />}
-        <div className="mb-16" />
         <PoweredBy />
         <TokenSelectionMenu />
       </div>
