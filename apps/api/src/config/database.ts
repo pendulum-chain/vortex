861310563a246065eea16d0e9c63060e4b5d31ab
Marcel Ebert <mail@marcel-ebert.de>
2026-05-20 11:22:05 +0200

    Add support for optional SSL CA certificate in Sequelize configuration

--- apps/api/src/config/database.ts
+++ apps/api/src/config/database.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from "node:fs";
 import { Sequelize } from "sequelize";
 import logger from "./logger";
 import { config } from "./vars";
@@ -17,18 +18,26 @@ declare module "./vars" {
   }
 }
 
+function getDialectOptions() {
+  if (config.env !== "production") {
+    return undefined;
+  }
+
+  const caCertPath = process.env.DB_SSL_CA_CERT_PATH;
+
+  return {
+    ssl: {
+      ...(caCertPath ? { ca: readFileSync(caCertPath, "utf8") } : {}),
+      rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== "false",
+      require: true
+    }
+  };
+}
+
 // Create Sequelize instance
 const sequelize = new Sequelize(config.database.database, config.database.username, config.database.password, {
   dialect: config.database.dialect,
-  dialectOptions:
-    config.env === "production"
-      ? {
-          ssl: {
-            rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== "false",
-            require: true
-          }
-        }
-      : undefined,
+  dialectOptions: getDialectOptions(),
   host: config.database.host,
   logging: config.database.logging ? msg => logger.debug(msg) : false,
   pool: {
